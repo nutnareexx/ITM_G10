eaba24d2b6e8c2f31806ca1d6cc79fe52f2d66d0
nutnareexx <nutnaree.co@ku.th>
2022-02-17 00:01:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,8 @@ else{
     <?php echo "controller = ".$controller.", action = ".$action ;?>
     <br>
     [<a href="?controller=pages&action=home"> HOME </a>]
-    [<a href="?controller=maxperday&action=index"> MAX_PER_DAY </a>]
-    <br>
-    [<a href="?controller=results&action=index"> RESULTS </a>]
-    [<a href="?controller=maxperday&action=index"> MAX_PER_DAY </a>]
 
+    [<a href="?controller=maxperday&action=index"> MAX_PER_DAY </a>]
     [<a href="?controller=results&action=index"> Results naaaaa </a>]
     [<a href="?controller=hospital&action=index"> HOSPITAL </a>]
     <?php require_once("routes.php");?>
