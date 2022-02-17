9ad58863430e33b66c53cedf28c0f270e25aa91b
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-02-17 22:58:56 +0700

    Create results_controller.php

---
+++ controllers/results_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+use LDAP\Result;
+
+class ResultsController
+{
+    public function index()
+    {
+        $resultsList = Results::getAll();
+        require_once('views/Results/index_results.php');
+    }
+}
+?>
