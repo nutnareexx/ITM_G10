55d8e8f96a8bfebe4a0a6ec3f0dd259aad62696c
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-02-18 00:22:30 +0700

    Update index_mexperday.php

--- views/mex_per_day/index_mexperday.php
+++ views/mex_per_day/index_mexperday.php
@@ -1,2 +1,15 @@
 <table border = 1 >
-    <tr> <td>Day</td> </tr>
+    <tr> <td>โรงพยาบาล</td> 
+    <td>วันที่</td>
+    <td>เวลาเปิดทำการ</td>
+    <td>เวลาปิดทำการ</td>
+    <td>จำนวนที่รับได้ต่อวัน</td>
+    <td>update</td>
+    <td>delete</td></tr>
+    <?php foreach($resultsList as $r)
+{
+    echo "<tr> <td>$r->Hid</td><td>$r->max_date</td><td>$r->max_topen</td><td>$r->max_tclose</td>
+    <td>$r->max_num</td><td>update</td><td>delete</td></tr>";
+}
+echo "</table> "
+?>
