1b6fc93aec530ed3e1e6af429be9839fba190440
nutnareexx <nutnaree.co@ku.th>
2022-02-24 00:21:54 +0700

    Update user_controller.php

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -22,7 +22,19 @@ class userModel{
     }
 
     public static function get($id){
-        
+        require("connection_connect.php");
+        $sql="SELECT u.user_id,nt.name_nt,u.user_name,u.user_surname,u.user_dateOfbirth,u.user_phone,u.user_mail 
+            FROM user AS u NATURAL JOIN names_title AS nt";
+        $result = $conn->query($sql);
+        $my_row = $result->fetch_assoc();
+        $uid = $my_row['user_id'];
+        $id_name = $my_row['name_nt'];
+        $uname = $my_row['user_name'];
+        $usurname = $my_row['name_surname'];
+        $udob = $my_row['name_dateOfbirth'];
+        $uphone = $my_row['name_phone'];
+        $umail = $my_row['name_mail'];
+
     }
 }
 ?>
