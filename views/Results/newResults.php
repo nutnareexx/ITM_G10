a78391031f459bd2535666ef2c764cfaa9e6f527
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-02-24 00:44:03 +0700

    Update newResults.php

--- views/Results/newResults.php
+++ views/Results/newResults.php
@@ -1,12 +1,14 @@
 <form method="get" action="">
     <label>รหัสผลตรวจ<input type="text" name="id"/></label><br>
     <label>ผลการตรวจ<input type="text" name="rs"/></label><br>
-    <label>รหัสผู้ป่วย<select name="uid">
-        <?php foreach($resultsList as $r) {echo "<option value = $r->uid>$r->uid</option>";}?>
+    <label>ผู้ป่วย<select name="uid">
+        <option value=""> choose </option>
+        <?php foreach($resultsList as $r) {echo "<option value = $r->uid>$r->name $r->sname</option>";}?>
         </select>
     </label><br>
-    <label>รหัสโรงพยาบาล<select name="hid">
-        <?php foreach($resultsList as $r) {echo "<option value = $r->hid>$r->hid</option>";}?>
+    <label>โรงพยาบาล<select name="hid">
+        <option value=""> choose </option>
+        <?php foreach($resultsList as $r) {echo "<option value = $r->hid>$r->hname</option>";}?>
         </select>
     </label><br>
     <input type="hidden" name="controller" value="results"/>
