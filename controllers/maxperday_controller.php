565e28d87b9a4f6286614f178d71816098d84f83
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-02-23 22:25:47 +0700

    Update maxperday_controller.php

--- controllers/maxperday_controller.php
+++ controllers/maxperday_controller.php
@@ -4,6 +4,7 @@ class maxperdayController
     public function index()
     {
         $maxperday_list = MaxperdayModel::getAll();
+        $hospitalmodelList = hospitalModel::getAll();
         require_once('views/mex_per_day/index_mexperday.php');
 
     }
