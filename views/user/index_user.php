c26a7915ecc17a7a583dc8d89296b23493086b7f
nutnareexx <nutnaree.co@ku.th>
2022-02-24 01:30:16 +0700

    Create index_user.php

---
+++ views/user/index_user.php
@@ -0,0 +1,10 @@
+<table border="1">
+    <tr>
+        <td> Pre Name </td>
+        <td> Name </td>
+        <td> Surname </td>
+        <td> Date of Birth</td>
+        <td> Phone </td>
+        <td> E-mail </td>
+    </tr>
+</table>
