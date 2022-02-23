0f33b6fbbd0e8f4826acc13ba29f92d66f5cfb7e
Potjarinn <potjarin.p@ku.th>
2022-02-23 22:28:14 +0700

    Update indexhospital.php

--- views/hospital/indexhospital.php
+++ views/hospital/indexhospital.php
@@ -1,15 +1,20 @@
 
-
+<form method="get" action="">
+    <input type="text" name="key">
+    <input type="hidden" name="controller" value="hospital"/>
+    <button type="submit" name="action" value="search"> Search </button>
+</form>
 <table border = 1>
 <tr>
-
+<td>รหัสโรงพยาบาล</td>
 <td>ชื่อโรงพยาบาล</td>
 <td>เวลาเปิดทำการ</td>
 <td>เวลาปิดทำการ</td>
 <td>ราคาตรวจ RT-PCR</td>
 <?php foreach( $hospitalmodelList as  $hospitallist)
 {
-    echo "<tr> <td>$hospitallist->H_name</td>
+    echo "<tr> <td>$hospitallist->Hid</td>
+    <td>$hospitallist->H_name</td>
     <td>$hospitallist->H_time_open</td>
     <td>$hospitallist->H_time_close</td> 
     <td>$hospitallist->H_price</td> 
