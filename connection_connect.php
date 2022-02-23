c64a38351666e04723379c1d817d3fa6fbb91ab8
nutnareexx <nutnaree.co@ku.th>
2022-02-23 19:41:06 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -1,9 +1,10 @@
 
 <?php
-$severname = "localhost";
-$username = "db21_129";
-$password = "db21_129";
+$servername = "localhost";
+$username = "root";
+$password = "";
 $dbname = "db21_129";
+
 
 $conn = new mysqli($servername,$username,$password) ;
 mysqli_set_charset($conn, "utf8");
@@ -12,10 +13,10 @@ if($conn->connect_error)
 {
   die("Connection failed:".$conn->connect_error);
 }
-
 if(!$conn->select_db($dbname))
 {
   die("Connection failed:" .$conn->connect_error);
 }
 
+echo "Hi";
 ?>
