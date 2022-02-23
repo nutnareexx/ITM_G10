53070e92d84e6de210516f73c095c89caa1ae410
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-02-24 01:27:43 +0700

    Update newMaxperday.php

---
+++ views/max_per_day/newMaxperday.php
@@ -0,0 +1,5 @@
+<form method="get" action = "">
+    <label> โรงพยาบาล <select name="H_name">
+        <?php foreach($hospitalmodelList as $hos)
+        {echo "<option value = $dep>"}
+    <label> วันที่ <input type = "date" name = "max_date" /> </label><br>
