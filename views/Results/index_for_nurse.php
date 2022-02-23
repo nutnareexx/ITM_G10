889de16edaa0128f2b4a72662a56674f56142182
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-02-23 22:25:40 +0700

    Update index_for_nurse.php

--- views/Results/index_for_nurse.php
+++ views/Results/index_for_nurse.php
@@ -3,18 +3,25 @@
 <br>new Results <a href="?controller=results&action=newResults"> CLICK!!! </a> <br>
 <br>
     <tr> <td>เลขบัตรประชาชน</td>
-    <td>ผลการตรวจ</td>
     <td>คำนำหน้า</td>
     <td>ชื่อ</td>
     <td>นามสกุล</td>
     <td>เบอร์โทรศัพท์</td>
     <td>อีเมลล์</td>
+    <td>โรงพยาบาล</td>
     <td>upload file</td>
     <td>update</td>
     <td>delete</td></tr>
 <?php foreach($resultsList as $r)
 {
-    echo "<tr> <td>$r->id</td><td>$r->rs</td><td>$r->uid</td><td>$r->hid</td>
+    echo "<tr> <td>$r->uid</td>
+    <td>$r->nt</td>
+    <td>$r->name</td>
+    <td>$r->sname</td>
+    <td>$r->phone</td>
+    <td>$r->mail</td>
+    <td>$r->hname</td>
+    <td>Upload File</td>
     <td>update</td><td>delete</td></tr>";
 }
 echo "</table> "
