c29d470ab9c9fbacafdd26ffb3458d51048ea2ad
nutnareexx <nutnaree.co@ku.th>
2022-02-24 23:40:54 +0700

    Update user_controller.php

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -8,15 +8,33 @@ class userController
         require_once('views/user/index_user.php');
     }
 
+    public function indexlogin(){
+        require_once('views/login/index_login.php');
+    }
+
+
     public function signin(){
         $prenameList = prenameModel::getAll();
         require_once('views/user/newuser.php');
     }
 
-    public function add(){
+    public function adduser(){
         $userList = userModel::getAll();
-
-        //foreach
+        $id = $_GET['uid'];
+        $preid = $_GET['preid'];
+        $name = $_GET['uname'];
+        $surname = $_GET['usurname'];
+        $phone = $_GET['uphone'];
+        $mail = $_GET['umail'];
+        foreach ($userList as $u){
+            if($u->uid=$id){
+                echo "have user";
+                break;
+            }
+            else{
+                echo "suceess";
+            }
+        }
     }
 
     
