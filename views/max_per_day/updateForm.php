32d4ef6d2165eda0d63484e78f89a213116a6832
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-02-25 01:39:25 +0700

    Update updateForm.php

--- views/max_per_day/updateForm.php
+++ views/max_per_day/updateForm.php
@@ -17,7 +17,9 @@
 
 
 <input type="hidden" name="controller" value="maxperday"/>
+<input type="hidden" name="max_id" value="<?php echo $m->max_id;?>"/>
+<input type="hidden" name="Hid" value="<?php echo $m->Hid;?>"/>
 <button type="submit" name="action" value="index">Back</button>
 <button type="submit" name="action" value="update"> update </button>
 
-</form>
+</form> 
