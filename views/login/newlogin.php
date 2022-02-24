8d4814ab80994c776700065c704f41fc48c11951
nutnareexx <nutnaree.co@ku.th>
2022-02-24 22:13:52 +0700

    Create newlogin.php

---
+++ views/login/newlogin.php
@@ -0,0 +1,12 @@
+<form method="get" action="">
+    <label> PASSWORD <input type="text" name="uname"/> </label>
+    <br>
+    <label> CONFIRM PASSWORD <input type="text" name="usurname"/> </label>
+    <br>
+    
+
+    <input type="hidden" name="controller" value="user"/>
+    <button type="submit" name="action" value="signin"> BACK </button>
+    <input type="hidden" name="controller" value="login"/>
+    <button type="submit" name="action" value="addpassword"> CONFIRM </button>
+</form>
