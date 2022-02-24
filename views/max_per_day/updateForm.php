4ef941558826d48ac4e296d221212ecd39cb11c1
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-02-24 23:42:07 +0700

    Update updateForm.php

---
+++ views/max_per_day/updateForm.php
@@ -0,0 +1,23 @@
+
+
+
+
+<form method="get"action="">
+    <br>
+    <?php echo "$m->H_name ";?> <br>
+    <label> วันที่ <input type="date" name="max_date"
+        value="<?php echo $m->max_date;?>"/></label><br>
+    <label> เวลาเปิดทำการ <input type="time" name="max_topen"
+        value="<?php echo $m->max_topen;?>"/></label><br>
+    <label> เวลาปิดทำการ <input type="time" name="max_tclose"
+        value="<?php echo $m->max_tclose;?>"/></label><br>
+    <label> จำนวนที่รับได้ <input type="text" name="max"
+        value="<?php echo $m->max_num;?>"/></label><br>
+       
+
+
+<input type="hidden" name="controller" value="maxperday"/>
+<button type="submit" name="action" value="index">Back</button>
+<button type="submit" name="action" value="update"> update </button>
+
+</form>
