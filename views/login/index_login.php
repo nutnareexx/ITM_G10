2e1e9652016c0e1e389158932e0eb0f1a825a0ca
nutnareexx <nutnaree.co@ku.th>
2022-02-24 19:53:27 +0700

    Create index_login.php

---
+++ views/login/index_login.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+    <form method="get" action="">
+        <br>
+        <label> USERNAME <input type="text" name="username"/> </label>
+        <br>
+        <label> PASSWORD <input type="text" name="password"/> </label>
+        <br>
+        <input type="hidden" name="controller" value="login"/>
+        <button type="submit" name="action" value="login"> LOG IN </button>
+        <input type="hidden" name="controller" value="user"/>
+        <button type="submit" name="action" value="signin"> SIGN IN </button>
+    </form>
+</body>
+</html>
