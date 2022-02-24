6b886691c1049aa0ab4486b2e3011814010cd848
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-02-24 22:11:54 +0700

    Update index_for_user.php

--- views/Results/index_for_user.php
+++ views/Results/index_for_user.php
@@ -1,7 +1,8 @@
 <table border=1>
 
 <br>
-    <tr> <td>เลขบัตรประชาชน</td>
+    <tr> <td>ลำดับที่</td>
+    <td>เลขบัตรประชาชน</td>
     <td>คำนำหน้า</td>
     <td>ชื่อ</td>
     <td>นามสกุล</td>
@@ -13,7 +14,8 @@
     </tr>
 <?php foreach($resultsList as $r)
 {
-    echo "<tr> <td>$r->uid</td>
+    echo "<tr> <td>$r->id</td>
+    <td>$r->uid</td>
     <td>$r->nt</td>
     <td>$r->name</td>
     <td>$r->sname</td>
