1943796409beb9583090d1e73d624a2a38e68557
nutnareexx <nutnaree.co@ku.th>
2022-02-24 20:25:22 +0700

    Update index_user.php

--- views/user/index_user.php
+++ views/user/index_user.php
@@ -7,4 +7,15 @@
         <td> Phone </td>
         <td> E-mail </td>
     </tr>
-</table>
+
+    <?php foreach($userList as $u){
+        echo "<tr> 
+        <td> $u->id_name</td>
+        <td> $u->uname</td>
+        <td> $u->usurname</td>
+        <td> $u->udob</td>
+        <td> $u->uphone</td>
+        <td> $u->umail</td>";
+    }
+    echo "</table>"
+?>
