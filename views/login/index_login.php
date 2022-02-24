73f9799e32afd8dcc137af31e9727031a6711cde
nutnareexx <nutnaree.co@ku.th>
2022-02-24 23:41:08 +0700

    Update index_login.php

--- views/login/index_login.php
+++ views/login/index_login.php
@@ -6,10 +6,9 @@
         <br><br>
         <label> PASSWORD <input type="text" name="password"/> </label>
         <br><br>
-        <input type="hidden" name="controller" value="login"/>
+        <input type="hidden" name="controller" value="user"/>
         <button type="submit" name="action" value="login"> LOG IN </button>
         <br><br>
-        <input type="hidden" name="controller" value="user"/>
         <button type="submit" name="action" value="signin"> SIGN IN </button>
     </form>
 </body>
