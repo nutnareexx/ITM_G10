4383d7344007ecac92103aee73837cee2ef2f4f2
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-02-24 22:11:51 +0700

    Update index_for_nurse.php

--- views/Results/index_for_nurse.php
+++ views/Results/index_for_nurse.php
@@ -2,25 +2,29 @@
 
 <br>new Results <a href="?controller=results&action=newResults"> CLICK!!! </a> <br>
 <br>
-    <tr> <td>เลขบัตรประชาชน</td>
+    <tr> <td>ลำดับที่</td>
+    <td>เลขบัตรประชาชน</td>
     <td>คำนำหน้า</td>
     <td>ชื่อ</td>
     <td>นามสกุล</td>
     <td>เบอร์โทรศัพท์</td>
     <td>อีเมลล์</td>
     <td>โรงพยาบาล</td>
+    <td>ผลการตรวจ</td>
     <td>upload file</td>
     <td>update</td>
     <td>delete</td></tr>
 <?php foreach($resultsList as $r)
 {
-    echo "<tr> <td>$r->uid</td>
+    echo "<tr> <td>$r->id</td>
+    <td>$r->uid</td>
     <td>$r->nt</td>
     <td>$r->name</td>
     <td>$r->sname</td>
     <td>$r->phone</td>
     <td>$r->mail</td>
     <td>$r->hname</td>
+    <td>$r->rs</td>
     <td>Upload File</td>
     <td><a href=?controller=results&action=updateForm&id=$r->uid> update </a></td>
     <td><a href=?controller=results&action=deleteConfirm&id=$r->uid> delete </a></td></tr>";
