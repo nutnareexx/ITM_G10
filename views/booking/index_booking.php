1c853b222d5523851eeb4e48d55f1a9d9bd53fd8
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2022-02-25 20:28:49 +0700

    Create index_booking.php

---
+++ views/booking/index_booking.php
@@ -0,0 +1,23 @@
+
+<head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<div class="container" style="max-width: 50%;">
+            <div class="text-center mt-5 mb-4">
+                <h2>การจองการตรวจRT-PCR</h2>
+            </div>
+
+            <input type="text" class="form-control" id="hospital" autocomplete="off"
+                placeholder="Search Hospital...">
+    </div>
+
+</html>
+
+
+<form method="get" action="">
+    <input type="text" name="key">
+    <input type="hidden" name="controller" value="maxperday"/>
+    <button type="submit" name="action" value="search"> 
+Search </button>
+</form>
