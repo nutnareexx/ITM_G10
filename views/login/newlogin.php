2ee9f38d4b41111d1684770b2744b17eb4a023a4
nutnareexx <nutnaree.co@ku.th>
2022-02-25 21:14:49 +0700

    Update newlogin.php

--- views/login/newlogin.php
+++ views/login/newlogin.php
@@ -1,12 +1,13 @@
 <form method="get" action="">
     <?php echo $id." password " ;?>
-    <label> PASSWORD <input type="text" name="uname"/> </label>
+    <label> PASSWORD <input type="text" name="password"/> </label>
     <br>
-    <label> CONFIRM PASSWORD <input type="text" name="usurname"/> </label>
+    <label> CONFIRM PASSWORD <input type="text" name="confirm"/> </label>
     <br>
     
 
     <input type="hidden" name="controller" value="user"/>
+    <input type="hidden" name="id" value="<?php echo $id; ?>"/>
     <button type="submit" name="action" value="signin"> BACK </button>
     <button type="submit" name="action" value="addpassword"> CONFIRM </button>
 </form>
