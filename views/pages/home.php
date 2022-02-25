3927ae53387297e9cd2bd90ecfce7fddd55dcfe7
nutnareexx <nutnaree.co@ku.th>
2022-02-26 01:55:35 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,7 +1,7 @@
 <html>
 <head> 
-    <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" >
+     
 </head>
 
 <!--<div class="container" style="max-width: 50%;">
@@ -13,18 +13,19 @@
 -->
 
 <style>
-    p.try1{
-        font-family: "Sofia", sans-serif;
-    }
-
     p.try2{
         font-family: 'Prompt', sans-serif;
     }
 </style>
+
 <body>
+<div class="container" style="max-width: 50%;">
+            <div class="text-center mt-5 mb-4">
+                <h2> การจองการตรวจRT-PCR </h2>
+            </div>
+
+</div>
     <br>
-    <h1> RT-PCR </h1>
-    <p class="try1"> RT-PCR </p>
     <h1> <p class="try2"> การจองการตรวจ RT-PCR </p> </h1>
 
     [<a href="?controller=booking&action=index"> การจอง </a>]
