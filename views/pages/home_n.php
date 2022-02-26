d05518081b10ea62dc20b592b53e9100a532441a
nutnareexx <nutnaree.co@ku.th>
2022-02-26 20:25:05 +0700

    Create home_n.php

---
+++ views/pages/home_n.php
@@ -0,0 +1,65 @@
+<html>
+<head> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" >
+    <style>
+        
+        /* Font */
+        .try2{
+            text-align:center;
+            font-family: 'Prompt', sans-serif;
+        }
+
+        .header {
+            font-family: 'Prompt', sans-serif;
+            background-color: #f1f1f1;
+            padding: 40px;
+            text-align: center;
+        }
+
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+    </style>
+</head>
+
+
+
+
+
+<body>
+    <br>
+
+    <div class="header">
+        <h1>  RT-PCR </h1>
+    </div>
+
+    <div class="topnav">
+    <a href="#"> วันเวลาที่รับตรวจ </a>
+    <a href="#"> ผลการตรวจ </a>
+    <!--<a href="?#"> โรงพยาบาล </a>-->
+
+    </div>
+
+</body>
+
+</html>
