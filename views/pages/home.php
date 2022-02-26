8d11860c18dacd0d3cc2ea95ba0addc579fc3b37
nutnareexx <nutnaree.co@ku.th>
2022-02-26 20:04:53 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -56,7 +56,7 @@
     <div class="topnav">
     <a href="?controller=booking&action=index"> จองตรวจ RT-PCR </a>
     <a href="?controller=booking&action=index"> ผลการตรวจ </a>
-    <a href="?controller=booking&action=index"> โรงพยาบาล </a>
+    <a href="?controller=hospital&action=index"> โรงพยาบาล </a>
 
     </div>
 
