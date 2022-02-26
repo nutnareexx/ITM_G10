74f0113767f6112601daec983578387c444eda90
nutnareexx <nutnaree.co@ku.th>
2022-02-26 19:55:08 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,35 +1,65 @@
 <html>
 <head> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" >
-     
+    <style>
+        
+        /* Font */
+        .try2{
+            text-align:center;
+            font-family: 'Prompt', sans-serif;
+        }
+
+        .header {
+            font-family: 'Prompt', sans-serif;
+            background-color: #f1f1f1;
+            padding: 40px;
+            text-align: center;
+        }
+
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+    </style>
 </head>
 
-<!--<div class="container" style="max-width: 50%;">
-            <div class="text-center mt-5 mb-4">
-                <h2> การจองการตรวจRT-PCR </h2>
-            </div>
 
-</div>
--->
 
-<style>
-    p.try2{
-        font-family: 'Prompt', sans-serif;
-    }
-</style>
+
 
 <body>
-<div class="container" style="max-width: 50%;">
-            <div class="text-center mt-5 mb-4">
-                <h2> การจองการตรวจRT-PCR </h2>
-            </div>
-
-</div>
     <br>
-    <h1> <p class="try2"> การจองการตรวจ RT-PCR </p> </h1>
 
-    [<a href="?controller=booking&action=index"> การจอง </a>]
-    <?php require_once("routes.php");?>
+    <div class="header">
+        <h1> จองการตรวจ RT-PCR </h1>
+    </div>
+
+    <div class="topnav">
+    <a href="?controller=booking&action=index"> จองตรวจ RT-PCR </a>
+    <a href="?controller=booking&action=index"> ผลการตรวจ </a>
+    <a href="?controller=booking&action=index"> โรงพยาบาล </a>
+
+    </div>
+
 </body>
 
 </html>
