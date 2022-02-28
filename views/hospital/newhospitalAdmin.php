12177482148e1799f4fac3eecb9ef0582409ee88
Potjarinn <potjarin.p@ku.th>
2022-03-01 00:16:18 +0700

    Update newhospitalAdmin.php

--- views/hospital/newhospitalAdmin.php
+++ views/hospital/newhospitalAdmin.php
@@ -2,8 +2,8 @@
     <br>
     <label>รหัสโรงพยาบาล<input type="text" name="H_name"/> </label><br>
     <label>ชื่อโรงพยาบาล<input type="text" name="H_name"/> </label><br>
-    <label>เวลาเปิดทำการ<input type="text" name="H_time_open"/> </label><br>
-    <label>เวลาปิดทำการ<input type="text" name="H_time_close"/> </label><br>
+    <label>เวลาเปิดทำการ<input type="time" name="H_time_open"/> </label><br>
+    <label>เวลาปิดทำการ<input type="time" name="H_time_close"/> </label><br>
     <label>ราคาตรวจ RT-PCR<input type="text" name="H_price"/> </label><br>
     
     <input type="hidden" name="controller" value="hospital"/>
