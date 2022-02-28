2938be30fcb1edd04415adf8f5e4218e7ca0f6c0
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-01 01:19:35 +0700

    Update uploadResults.php

--- views/Results/uploadResults.php
+++ views/Results/uploadResults.php
@@ -1,3 +1,4 @@
+<html>
 
 <?php 
 include('connect.php');
@@ -8,22 +9,22 @@ $result = mysqli_query($con, $query);
 mysqli_close($con);
 ?>
 <br/>
+
+
+
 <form action="add_file_db.php" method="post" enctype="multipart/form-data" name="upfile" id="upfile">
-  <!--<p>&nbsp;</p>-->
- 
-    
-    <tr>
+
     <label>
-        <input type="file" name="fileupload" id="fileupload"  required="required"/><br>
-      </label></td>
-    </tr>
-    <tr>
+      <input type="file" name="fileupload" id="fileupload"  required="required"/><br>
+    </label>
     <br>
-    <td><input type="submit" name="button" id="button" value="Upload" /></td>
-    </tr>
-
-  </table>
-  <!--<p>&nbsp;</p>-->
-</form>
-
+    <input type="hidden" name="controller" value="results"/>
+    <input type="hidden" name="id" value="<?php echo $id;?>" required="required"/>
+    <button type="submit" name="button" id="button" value="upResult"> SUBMIT </button>
     
+</form>
+<form method="get" action="">
+  <input type="hidden" name="controller" value="results"/>
+  <button type="submit" name="action" value="index">Back</button>
+</form>
+</html>
