b83534b9feaa4b4405c39aea57af10aac866c69c
nutnareexx <nutnaree.co@ku.th>
2022-03-01 03:18:56 +0700

    Update uploadResults.php

--- views/Results/uploadResults.php
+++ views/Results/uploadResults.php
@@ -1,30 +1,26 @@
 <html>
 
-<?php 
-include('connect.php');
-$query = "SELECT * FROM uploadfile ORDER BY fileID asc" or die("Error:" . mysqli_error($con));
-$result = mysqli_query($con, $query); 
-
-//5. close connection
-mysqli_close($con);
-?>
-<br/>
+  <?php 
+    //require("connection_connect.php");
+    //$result = $conn->query($sql); 
+  ?>
 
 
 
-<form action="add_file_db.php" method="post" enctype="multipart/form-data" name="upfile" id="upfile">
+  <form action="add_file_db.php" method="post" enctype="multipart/form-data" name="upfile" id="upfile">
 
-    <label>
-      <input type="file" name="fileupload" id="fileupload"  required="required"/><br>
-    </label>
-    <br>
+      <label>
+        <input type="file" name="fileupload" id="fileupload"  required="required"/><br>
+      </label>
+      <br>
+      <!--<input type="hidden" name="controller" value="results"/>-->
+      <input type="hidden" name="id" value="<?php echo $re->id;?>"/>
+      <input type="hidden" name="uid" value="<?php echo $re->uid;?>"/>
+      <button type="submit" name="button" id="button" value="upResult"> SUBMIT </button>
+      
+  </form>
+  <form method="get" action="">
     <input type="hidden" name="controller" value="results"/>
-    <input type="hidden" name="id" value="<?php echo $id;?>" required="required"/>
-    <button type="submit" name="button" id="button" value="upResult"> SUBMIT </button>
-    
-</form>
-<form method="get" action="">
-  <input type="hidden" name="controller" value="results"/>
-  <button type="submit" name="action" value="index">Back</button>
-</form>
+    <button type="submit" name="action" value="index">Back</button>
+  </form>
 </html>
