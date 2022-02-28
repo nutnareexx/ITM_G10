6aa72f6191e3a82f6bc8454f110693becc1dde84
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-01 03:20:47 +0700

    Update index_booking.php

--- views/booking/index_booking.php
+++ views/booking/index_booking.php
@@ -29,45 +29,72 @@
         
     }
 
-    p.try2{
+    .try2{
         font-family: 'Prompt', sans-serif;
         padding: 20px;
         text-align: center;
     }
 
-    .button {
-        background-color: #4CAF50;
-        border: none;
+    .btn-group .button {
+        /*border-radius: 5%;*/
+        position: relative;
+        background-color: #333 ; /* Green */
+        border: 1px black;
         color: white;
-        padding: 10px 25px;
+        padding: 15px 32px;
         text-align: center;
         text-decoration: none;
         display: inline-block;
         font-size: 16px;
-        margin: 4px 2px;
         cursor: pointer;
+        /*float: center;*/
     }
+
+    .btn-group .button:not(:last-child) {
+        border-right: none; /* Prevent double borders */
+    }
+
+    .btn-group .button:hover {
+        background-color: #ddd;
+        color: black;
+    }
+
+    input[type=text], select {
+        font-family: 'Prompt', sans-serif;
+        width: 25%;
+        padding: 12px 10px;
+        margin: 8px 0;
+        font-size: 16px;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    
+    
 </style>
 
 <body>
     <br>
-    <h1> <p class="try2"> การจองการตรวจ RT-PCR </p> </h1>
-
-
+    <div class="try2"><h1>  การจองการตรวจ RT-PCR </h1></div>
     
-    <form method="get" action="">
+
+
+
     <div class="try1">
-    <input type="text" name="key">
-    
-    <div class="button">
-        <input type="hidden" name="controller" value="booking"/>
-            <button type="submit" name="action" value="search"> 
-        Search </button>
+        <form method="get" action="">
+            <input type="text" name="key">
+            <div class="btn-group">
+                <input type="hidden" name="controller" value="booking"/>
+                
+                <button class="button" type="submit" name="action" value="search"> Search </button>
+                
+            </div>
+        </form>   
     </div>
-        
 
-</form>
-</div>
+    
+
 
     
     <div class="try1">
