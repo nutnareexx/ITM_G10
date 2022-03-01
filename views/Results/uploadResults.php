17e0267ec1952994d2ea5e478e6991bd0ed9b1be
nutnareexx <nutnaree.co@ku.th>
2022-03-02 06:01:17 +0700

    Update uploadResults.php

--- views/Results/uploadResults.php
+++ views/Results/uploadResults.php
@@ -1,12 +1,66 @@
 <html>
+  <head> 
+        <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+  </head>
+ 
+  <style>
+    .header {
+                font-family: 'Prompt', sans-serif;
+                background-color: #f1f1f1;
+                padding: 20px;
+                text-align: center;
+            }
 
-  <?php 
-    //require("connection_connect.php");
-    //$result = $conn->query($sql); 
-  ?>
+            .btn-group .button {
+                font-family: 'Prompt', sans-serif;
+                position: relative;
+                background-color: #0974ba ; /* Green */
+                border: 1px black;
+                color: white;
+                padding: 15px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                cursor: pointer;
+                /*float: center;*/
+            }
+
+            .btn-group .button:not(:last-child) {
+                border-right: none; /* Prevent double borders */
+            }
+
+            .btn-group .button:hover {
+                background-color: #ddd;
+                color: black;
+            }
+
+            .card2 {
+                font-family: 'Prompt', sans-serif;
+                background-color: #f1f1f1;
+                padding: 20px;
+                margin-top: 20px;
+               
+            }
+            input[type=file], select {
+                font-family: 'Prompt', sans-serif;
+                width: 100%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                box-sizing: border-box;
+            }
+  </style>
+
+<div class="header">
+  <h1> จองตรวจ RT-PCR 
+  </h1>
+</div>
 
 
-
+<div class="card2">
   <form action="add_file_db.php" method="post" enctype="multipart/form-data" name="upfile" id="upfile">
 
       <label>
@@ -14,13 +68,19 @@
       </label>
       <br>
       <!--<input type="hidden" name="controller" value="results"/>-->
+      <div class="btn-group">
       <input type="hidden" name="id" value="<?php echo $re->id;?>"/>
       <input type="hidden" name="uid" value="<?php echo $re->uid;?>"/>
-      <button type="submit" name="button" id="button" value="upResult"> SUBMIT </button>
-      
+      <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
+      <button class="button" type="submit" name="button" id="button" value="upResult"> SUBMIT </button>
+      </div>
   </form>
   <form method="get" action="">
+    <div class="btn-group">
     <input type="hidden" name="controller" value="results"/>
-    <button type="submit" name="action" value="index">Back</button>
+    <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
+    <button class="button" type="submit" name="action" value="indexNurse">Back</button>
+    </div>
   </form>
+</div>
 </html>
