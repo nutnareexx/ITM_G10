b2cbdccdca781ff0b9ebabb053611f4d663e4e76
nutnareexx <nutnaree.co@ku.th>
2022-03-02 06:00:49 +0700

    Update deleteconfirmAdmin.php

--- views/hospital/deleteconfirmAdmin.php
+++ views/hospital/deleteconfirmAdmin.php
@@ -13,7 +13,7 @@
         }
         .btn-group .button {
             position: relative;
-            background-color: #333 ; /* Green */
+            background-color: #0974ba ; /* Green */
             border: 1px black;
             color: white;
             padding: 10px 32px;
