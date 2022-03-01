825ce90a2552d89111f15fdeeec4b54b12ab6961
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-02 00:12:32 +0700

    Update indexhospital.php

--- views/hospital/indexhospital.php
+++ views/hospital/indexhospital.php
@@ -1,68 +1,160 @@
-<form method="get" action="">
-    <input type="text" name="key">
-    <input type="hidden" name="controller" value="hospital"/>
-    <button type="submit" name="action" value="search"> Search </button>
-</form>
-<table border = 1>
-<tr>
-<td><b>ชื่อโรงพยาบาล</td>
-<td><b>เวลาเปิดทำการ</td>
-<td><b>เวลาปิดทำการ</td>
-<td><b>ราคาตรวจ RT-PCR</td>
-<?php foreach( $hospitalmodelList as $hospitallist)
-{
-    echo "<tr> <td>$hospitallist->H_name</td>
-    <td>$hospitallist->H_time_open</td>
-    <td>$hospitallist->H_time_close</td> 
-    <td>$hospitallist->H_price</td> 
-    </td></tr>";
-}
-echo "</table>";
- ?>
-
-
-<!DOCTYPE html>
 <html>
-<head>
-    <title>Hospital</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
+    <head> 
+        <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
+    </head>
 
-<body>
-    <div class="container" style="max-width: 50%;">
-            <div class="text-center mt-5 mb-4">
-                <h2>THG Hospital</h2>
-            </div>
+    <style>
+        .header {
+                font-family: 'Prompt', sans-serif;
+                background-color: #f1f1f1;
+                padding: 40px;
+                text-align: center;
+            }
 
-            <input type="text" class="form-control" id="hospital" autocomplete="off"
-                placeholder="Search Hospital...">
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+        
+        #customers {
+            font-family: 'Prompt', sans-serif;
+            border-collapse: collapse;
+            width: 80%;
+            }
+
+        #customers td, #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+            }
+
+        #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+        #customers tr:hover {background-color: #ddd;}
+
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: center;
+            background-color: #333;
+            color: white;
+            }
+
+
+        .try1{
+            font-family: 'Prompt', sans-serif;
+            text-align: center;
+            
+        }
+
+        .try2{
+            font-family: 'Prompt', sans-serif;
+            padding: 20px;
+            text-align: center;
+        }
+
+        .btn-group .button {
+            /*border-radius: 5%;*/
+            position: relative;
+            background-color: #333 ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            /*float: center;*/
+        }
+
+        .btn-group .button:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group .button:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        input[type=text], select {
+            font-family: 'Prompt', sans-serif;
+            width: 25%;
+            padding: 12px 10px;
+            margin: 8px 0;
+            font-size: 16px;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+        
+        
+    </style>
+
+    <div class="header">
+        <h1> โรงพยาบาล </h1>
     </div>
 
-    <div id="searchresult"></div>
+    <div class="topnav">
+                <a href="?controller=booking&action=index&userid=<?php echo $userid?>"> จองตรวจ RT-PCR </a>
+                <a href="?controller=results&action=indexUser&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+                <a href="?controller=hospital&action=indexUser&userid=<?php echo $userid?>"> โรงพยาบาล </a>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $("#hospital").keyup(function(){
-                var input = $(this).val();
-                //alert(input);
+                <?php echo $userid?>
+            </div>
+            <br>
 
-                if(input != ""){
-                    $.ajax({
+    <body>
+        <center>
+                <form method="get" action="">
+                <div class="btn-group">
+                <input type="text" name="key">
+                    <input type="hidden" name="controller" value="hospital"/>
+                    <button class="button" type="submit" name="action" value="search"> Search </button>
+                </div>    
+            </form>
+            <table border = 1 id="customers">
+            <tr>
+            <th><b>ชื่อโรงพยาบาล</th>
+            <th><b>เวลาเปิดทำการ</th>
+            <th><b>เวลาปิดทำการ</th>
+            <th><b>ราคาตรวจ RT-PCR</th>
+            <?php foreach( $hospitalmodelList as $hospitallist)
+            {
+                echo "<tr> <td>$hospitallist->H_name</td>
+                <td>$hospitallist->H_time_open</td>
+                <td>$hospitallist->H_time_close</td> 
+                <td>$hospitallist->H_price</td> 
+                </td></tr>";
+            }
+            echo "</table>";
+            ?>
+        </center>
+        
+    </body>
+    
 
-                        url:"livesearch.php",
-                        method:"POST",
-                        data:{input:input},
-
-                        success:function(data){
-                            $("#searchresult").html(data);
-                        }
-                    });
-                }else{
-                    $("#searchresult").css("display","none");
-                }
-            });
-        });
-    </script>
-</body>
 </html>
+
+
+
+
