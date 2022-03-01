7704108d7a484dc38665f54e5be500f53499edde
nutnareexx <nutnaree.co@ku.th>
2022-03-02 00:43:42 +0700

    Update index_booking.php

--- views/booking/index_booking.php
+++ views/booking/index_booking.php
@@ -6,68 +6,8 @@
         
     </head>
 `
-    <style>
-        .try1{
-            font-family: 'Prompt', sans-serif;
-            text-align: center;
-            
-        }
+    
 
-        .try2{
-            font-family: 'Prompt', sans-serif;
-            padding: 20px;
-            text-align: center;
-        }
-
-        .btn-group .button {
-            /*border-radius: 5%;*/
-            position: relative;
-            background-color: #333 ; /* Green */
-            border: 1px black;
-            color: white;
-            padding: 15px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            cursor: pointer;
-            /*float: center;*/
-        }
-
-        .btn-group .button:not(:last-child) {
-            border-right: none; /* Prevent double borders */
-        }
-
-        .btn-group .button:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-        input[type=text], select {
-            font-family: 'Prompt', sans-serif;
-            width: 25%;
-            padding: 12px 10px;
-            margin: 8px 0;
-            font-size: 16px;
-            display: inline-block;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            box-sizing: border-box;
-        }
-        
-        
-    </style>
-
-    <body>
-        <br>
-        <div class="try2"><h1>  การจองการตรวจ RT-PCR </h1></div>
-        
-
-<html>
-<head> 
-    <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
-</head>
 
 <style>
     .header {
@@ -168,30 +108,16 @@
 
 
     <div class="try1">
-        <form method="get" action=""><div class="btn-group">
+        <div class="btn-group">
+        <form method="get" action="">
             <input type="text" name="key">
-                <input type="hidden" name="controller" value="booking"/>
-                <button class="button" type="submit" name="action" value="search"> Search </button>
-                
-            </div>
-        </form>   
-
-
-        <div class="try1">
-            <form method="get" action="">
-                <input type="text" name="key">
-                <div class="btn-group">
-                    <input type="hidden" name="controller" value="booking"/>
-                    
-                    <button class="button" type="submit" name="action" value="search"> Search </button>
-                    
-                </div>
-            </form>   
-        </div>
+            <input type="hidden" name="controller" value="booking"/>
+            <input type="hidden" name="userid" value="<?php echo $userid?>"/>
+            <button class="button" type="submit" name="action" value="search"> Search </button>
+        </form> 
+      </div>
 
         
-
-
         
         <div class="try1">
         <?php foreach($hospitalmodelList as $hospital)
@@ -207,8 +133,9 @@
         
         
         }
-    echo "</table> "
-    ?>
+        echo "</table> "
+        ?>
+        </div>
     </div>
         
 
