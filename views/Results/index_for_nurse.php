ff0a385321a01649f555a57c3569595d0cac9052
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-01 15:03:28 +0700

    Update index_for_nurse.php

--- views/Results/index_for_nurse.php
+++ views/Results/index_for_nurse.php
@@ -60,7 +60,7 @@
             {
                 echo "<tr> <td>$r->id</td>
                 <td>$r->uid</td>
-                <td>$r->nt</td>
+                <td>$r->pname</td>
                 <td>$r->name</td>
                 <td>$r->sname</td>
                 <td>$r->phone</td>
