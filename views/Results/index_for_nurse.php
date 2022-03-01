790ac5ca11dc389780f2c58255d8c910d8a48e7a
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-02 00:12:44 +0700

    Update index_for_nurse.php

--- views/Results/index_for_nurse.php
+++ views/Results/index_for_nurse.php
@@ -4,6 +4,48 @@
     </head>
 
     <style>
+        .header {
+            font-family: 'Prompt', sans-serif;
+            background-color: #f1f1f1;
+            padding: 40px;
+            text-align: center;
+        }
+
+        .try1{
+            font-family: 'Prompt', sans-serif;
+            text-align: center;
+            
+        }
+
+        .try2{
+            font-family: 'Prompt', sans-serif;
+            padding: 20px;
+            text-align: center;
+        }
+
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
         #customers {
             font-family: 'Prompt', sans-serif;
             border-collapse: collapse;
@@ -25,19 +67,71 @@
             text-align: left;
             background-color: #333;
             color: white;
-            }
+        }
+
+        .btn-group .button {
+            /*border-radius: 5%;*/
+            position: relative;
+            background-color: #333 ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            /*float: center;*/
+        }
+
+        .btn-group .button:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group .button:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        input[type=text], select {
+            font-family: 'Prompt', sans-serif;
+            width: 25%;
+            padding: 12px 10px;
+            margin: 8px 0;
+            font-size: 16px;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
             
     </style>
 
+<div class="header">
+        <h1> ผลการตรวจ RT-PCR </h1>
+</div>
+
+<div class="topnav">
+        <a href="?controller=booking&action=index&userid=<?php echo $userid?>"> จองตรวจ RT-PCR </a>
+        <a href="?controller=results&action=indexUser&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+        <a href="?controller=hospital&action=indexUser&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+
+        <?php echo $userid?>
+</div><br>
+
  <div class="center">
-            <table border=1 id="customers"><br>
+     <center>
+         <div class="try1">
+            <table border=1 id="customers">
 
             <br>new Results <a href="?controller=results&action=newResultsNurse"> CLICK!!! </a> <br>
             <br><form method="get" action="">
+            <div class="btn-group">
                 <input type="text" name="key">
                 <input type="hidden" name="controller" value="results"/>
-                <button type="submit" name="action" value="searchNurse"> 
+                <button class="button" type="submit" name="action" value="searchNurse"> 
             Search </button>
+            </div>
             </form><br>
 
 
@@ -73,6 +167,10 @@
             }
             echo "</table> "
             ?>
+         </div>
+         
+     </center>
+            
             <!--<form action="/action_page.php">
                 
                     <input type="file" id="myfile" name="myfile"><br><br>
