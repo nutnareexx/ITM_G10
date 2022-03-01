216d72f3797831636342cbc0d96374f17c885468
nutnareexx <nutnaree.co@ku.th>
2022-03-02 01:20:29 +0700

    Update checkbook.php

--- views/booking/checkbook.php
+++ views/booking/checkbook.php
@@ -1,9 +1,9 @@
-<?php echo "<br><br>ต้องการจอง<br>
-<br>โรงพยาบาล : $booking->H_name
-<br>วันที่ : $booking->max_date
-<br>เวลาเปิดทำการ : $booking->max_topen
-<br>เวลาปิดทำการ : $booking->max_tclose
-";?>
+<?php 
+    echo "<br><br>ต้องการจอง<br>
+    <br>โรงพยาบาล : $booking->H_name
+    <br>วันที่ : $booking->max_date
+    <br>เวลาเปิดทำการ : $booking->max_topen - $booking->max_tclose";
+?>
 <form method="get" action="">
     <input type="hidden" name="controller" value="booking"/>
     <input type="hidden" name="max_id" value="<?php echo $booking->max_id;?>"/>
