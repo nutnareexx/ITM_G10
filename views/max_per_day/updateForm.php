00927194696b0a4ef89a6b7c8a09f56c441a3f40
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-02 03:15:27 +0700

    Update updateForm.php

--- views/max_per_day/updateForm.php
+++ views/max_per_day/updateForm.php
@@ -1,25 +1,87 @@
+<html>
+<head> 
+        <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+    </head>
+
+    <style>
+
+        .card {
+            font-family: 'Prompt', sans-serif;
+            background-color: #f1f1f1;
+            padding: 20px;
+            margin-top: 20px;
+        }
+        .btn-group .button {
+            position: relative;
+            background-color: #333 ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            /*float: center;*/
+        }
+
+        .btn-group .button:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group .button:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        input[type=text], select {
+            font-family: 'Prompt', sans-serif;
+            width: 15%;
+            padding: 10px 10px;
+            margin: 8px 0;
+            font-size: 16px;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+
+        .center {
+            text-align: center;
+            border: none;
+            }
+    </style>
+
+<div class="card center">
+
+    <form method="get"action="">
+        <div class="btn-group">
+        <br>
+        <?php echo "$m->H_name ";?> <br>
+        <label> วันที่ <input type="date" name="max_date"
+            value="<?php echo $m->max_date;?>"/></label><br>
+        <label> เวลาเปิดทำการ <input type="time" name="max_topen"
+            value="<?php echo $m->max_topen;?>"/></label><br>
+        <label> เวลาปิดทำการ <input type="time" name="max_tclose"
+            value="<?php echo $m->max_tclose;?>"/></label><br>
+        <label> จำนวนที่รับได้ <input type="text" name="max"
+            value="<?php echo $m->max_num;?>"/></label><br>
+        
+
+    
+        <input type="hidden" name="controller" value="maxperday"/>
+        <input type="hidden" name="max_id" value="<?php echo $m->max_id;?>"/>
+        <input type="hidden" name="Hid" value="<?php echo $m->Hid;?>"/>
+
+        <button class="button" type="submit" name="action" value="index">Back</button>
+        <button class="button" type="submit" name="action" value="update"> update </button>
+
+    </div>
+    
+</div>
+        
+    </form> 
+</html>
 
 
 
-
-<form method="get"action="">
-    <br>
-    <?php echo "$m->H_name ";?> <br>
-    <label> วันที่ <input type="date" name="max_date"
-        value="<?php echo $m->max_date;?>"/></label><br>
-    <label> เวลาเปิดทำการ <input type="time" name="max_topen"
-        value="<?php echo $m->max_topen;?>"/></label><br>
-    <label> เวลาปิดทำการ <input type="time" name="max_tclose"
-        value="<?php echo $m->max_tclose;?>"/></label><br>
-    <label> จำนวนที่รับได้ <input type="text" name="max"
-        value="<?php echo $m->max_num;?>"/></label><br>
-       
-
-
-<input type="hidden" name="controller" value="maxperday"/>
-<input type="hidden" name="max_id" value="<?php echo $m->max_id;?>"/>
-<input type="hidden" name="Hid" value="<?php echo $m->Hid;?>"/>
-<button type="submit" name="action" value="index">Back</button>
-<button type="submit" name="action" value="update"> update </button>
-
-</form> 
