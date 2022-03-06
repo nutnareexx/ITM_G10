2ba780c163163bbdb31072a6be5bc670b369c2ba
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-06 23:57:34 +0700

    Update home_n.php

--- views/pages/home_n.php
+++ views/pages/home_n.php
@@ -1,6 +1,7 @@
 <html>
 <head> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" >
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <style>
         
         /* Font */
@@ -57,6 +58,8 @@
     <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
     <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
     <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+    <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
+        <i class='fas fa-user-alt' style='font-size:24px'></i></a>
 
     </div>
 
