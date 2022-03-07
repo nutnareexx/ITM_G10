9a1dbc30546902666f6a5c461dc555e5ff8d177b
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-08 02:22:40 +0700

    Update deleteconfirmAdmin.php

--- views/hospital/deleteconfirmAdmin.php
+++ views/hospital/deleteconfirmAdmin.php
@@ -1,15 +1,41 @@
 <html>
 <head> 
         <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
     </head>
 
     <style>
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color:#0974ba;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
 
         .card {
             font-family: 'Prompt', sans-serif;
             background-color: #f1f1f1;
             padding: 20px;
-            margin-top: 20px;
+            
         }
         .btn-group .button {
             position: relative;
@@ -23,6 +49,7 @@
             font-size: 16px;
             cursor: pointer;
             /*float: center;*/
+            border-radius: 50px;
         }
 
         .btn-group .button:not(:last-child) {
@@ -36,13 +63,13 @@
 
         input[type=password], select {
             font-family: 'Prompt', sans-serif;
-            width: 15%;
+            width: 100%;
             padding: 10px 10px;
             margin: 8px 0;
             font-size: 16px;
             display: inline-block;
             border: 1px solid #ccc;
-            border-radius: 4px;
+            border-radius: 50px;
             box-sizing: border-box;
         }
 
@@ -50,23 +77,59 @@
             text-align: center;
             border: none;
             }
+
+        table, td {
+            height: 40px;
+        }
     </style>
 
+<div class="topnav">
+        <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
+        <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+        <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+        <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
+        <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+
+        <?php echo $userid?>
+</div>
+
+
 <div class="card center">
-    <div class="btn-group">
-        <?php echo "<br><br>Are you sure to delete this Hospital? <br>
-                    <br>  รหัสโรงพยาบาล:$hospitalmodels->Hid
-                    <br>ชื่อโรงพยาบาล:$hospitalmodels->H_name  
-                    <br>เวลาเปิดทำการ:$hospitalmodels->H_time_open 
-                    <br>เวลาปิดทำการ:$hospitalmodels->H_time_close  
-                    <br>ราคาตรวจ RT-PCR:$hospitalmodels->H_price<br>";?>
-        <form method="get" action=""><br>
+        <table align="center">
+            <tr>
+                <td align="right">รหัสโรงพยาบาล : </td>
+                <td><?php echo "$hospitalmodels->Hid"?></td>
+            </tr>
+            <tr>
+                <td align="right">ชื่อโรงพยาบาล : </td>
+                <td><?php echo "$hospitalmodels->H_name"?></td>
+            </tr>
+            <tr>
+                <td align="right">เวลาเปิดทำการ : </td>
+                <td><?php echo "$hospitalmodels->H_time_open"?></td>
+            </tr>
+            <tr>
+                <td align="right">เวลาปิดทำการ : </td>
+                <td><?php echo "$hospitalmodels->H_time_close"?></td>
+            </tr>
+            <tr>
+                <td align="right">ราคาตรวจ RT-PCR : </td>
+                <td><?php echo "$hospitalmodels->H_price"?></td>
+            </tr>
+            
+
+        </table>
+        <br>
+        <div class="btn-group">
+            <form method="get" action="">
             <input type="hidden" name="controller" value="hospital"/>
             <input type="hidden" name="Hidnew" value="<?php echo $hospitalmodels->Hid;?>"/>
             <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
             <button class="button" type="submit" name="action" value="indexAdmin">Back</button>
             <button class="button" type="submit" name="action" value="delete">Delete</button>
         </form>
+        </div>
+        
 
 </div>
     
