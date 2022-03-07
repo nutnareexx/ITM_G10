74bde82fe7ffb8361dba31d53d736ab8413fc889
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-08 02:23:02 +0700

    Update deleteConfirm.php

--- views/max_per_day/deleteConfirm.php
+++ views/max_per_day/deleteConfirm.php
@@ -1,28 +1,55 @@
 <html>
 <head> 
         <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
     </head>
 
     <style>
+         /* Style the top navigation bar */
+         .topnav {
+            overflow: hidden;
+            background-color:#0974ba;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
 
         .card {
             font-family: 'Prompt', sans-serif;
             background-color: #f1f1f1;
             padding: 20px;
-            margin-top: 20px;
+            
         }
         .btn-group .button {
             position: relative;
             background-color: #0974ba; /* Green */
             border: 1px black;
             color: white;
-            padding: 15px 32px;
+            padding: 10px 32px;
             text-align: center;
             text-decoration: none;
             display: inline-block;
             font-size: 16px;
             cursor: pointer;
             /*float: center;*/
+            border-radius: 50px;
         }
 
         .btn-group .button:not(:last-child) {
@@ -42,7 +69,7 @@
             font-size: 16px;
             display: inline-block;
             border: 1px solid #ccc;
-            border-radius: 4px;
+            border-radius: 50px;
             box-sizing: border-box;
         }
 
@@ -50,16 +77,47 @@
             text-align: center;
             border: none;
             }
+
+        table, td {
+            height: 40px;
+        }
     </style>
 
+<div class="topnav">
+        <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
+        <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+        <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+        <a href="?controller=user&action=indexAdmin&userid=<?php echo $userid?>" style="float:right">
+        <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+
+        <?php echo $userid?>
+</div>
+
     <div class="card center">
-        <div class="btn-group">
-            <?php echo "<br><br>Are you Sure to delete this id?<br>
-            <br>โรงพยาบาล : $m->H_name
-            <br>วันที่ : $m->max_date
-            <br>เวลาเปิดทำการ : $m->max_topen
-            <br>เวลาปิดทำการ : $m->max_tclose
-            <br>จำนวนที่รับได้ต่อวัน : $m->max_num";?>
+        <table align="center">
+            <tr>
+                <td align="right">โรงพยาบาล : </td>
+                <td><?php echo "$m->H_name"?></td>
+            </tr>
+            <tr>
+                <td align="right">วันที่ : </td>
+                <td><?php echo "$m->max_date"?></td>
+            </tr>
+            <tr>
+                <td align="right">เวลาเปิดทำการ : </td>
+                <td><?php echo "$m->max_topen"?></td>
+            </tr>
+            <tr>
+                <td align="right">เวลาปิดทำการ : </td>
+                <td><?php echo "$m->max_tclose"?></td>
+            </tr>
+            <tr>
+                <td align="right">จำนวนที่รับได้ต่อวัน : </td>
+                <td><?php echo "$m->max_num"?></td>
+            </tr>
+        </table>
+          <br>
+        <div class="btn-group">    
             <form method="get" action="">
                 <input type="hidden" name="controller" value="maxperday"/>
                 <input type="hidden" name="max_id" value="<?php echo $m->max_id;?>"/>
