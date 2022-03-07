b41891671b11c0018d166ae6ed7a01593094c39b
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-08 02:23:24 +0700

    Update newResults.php

--- views/Results/newResults.php
+++ views/Results/newResults.php
@@ -1,15 +1,40 @@
 <html>
     <head> 
         <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
     </head>
 
     <style>
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color:#0974ba;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
 
         .card {
             font-family: 'Prompt', sans-serif;
             background-color: #f1f1f1;
             padding: 20px;
-            margin-top: 20px;
+            
         }
         .btn-group .button {
             position: relative;
@@ -23,6 +48,7 @@
             font-size: 16px;
             cursor: pointer;
             /*float: center;*/
+            border-radius: 50px;
         }
 
         .btn-group .button:not(:last-child) {
@@ -36,13 +62,13 @@
 
         input[type=text], select {
             font-family: 'Prompt', sans-serif;
-            width: 15%;
+            width: 100%;
             padding: 5px 5px;
             margin: 8px 0;
             font-size: 16px;
             display: inline-block;
             border: 1px solid #ccc;
-            border-radius: 4px;
+            border-radius: 50px;
             box-sizing: border-box;
         }
 
@@ -52,30 +78,54 @@
             }
     </style>
 
-    <div class="card center">
+<div class="topnav">
+    <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
+    <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+    <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+    <a href="?controller=user&action=indexAdmin&userid=<?php echo $userid?>" style="float:right">
+    <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+
+                <?php echo $userid?>
+</div>
+
+    <div class="card">
         <form method="get" action="">
-            <div class="btn-group">
-            <label>รหัสผลตรวจ <input type="text" name="id"/></label><br>
+            <table align="center">
+                <tr>
+                    <td align="right">รหัสผลตรวจ : </td>
+                    <td><label> <input type="text" name="id"/></label></td>
+                </tr>
+                <tr>
+                    <td align="right">ผู้ป่วย : </td>
+                    <td><label> <select name="uid">
+                    <option value=""> choose </option>
+                    <?php foreach($user as $u) {echo "<option value = $u->uid>$u->uname $u->usurname</option>";}?>
+                    </select>
+                    </label></td>
+                </tr>
+                <tr>
+                    <td align="right">ผลการตรวจ : </td>
+                    <td><label> <input type="text" name="rs"/></label></td>
+                </tr>
+                <tr>
+                    <td align="right">โรงพยาบาล : </td>
+                    <td><label> <select name="hid">
+                        <option value=""> choose </option>
+                        <?php foreach($h_List as $h) {echo "<option value = $h->Hid>$h->H_name</option>";}?>
+                        </select>
+                    </label></td>
+                </tr>
+                
             </div>
-            
-            <label>ผู้ป่วย <select name="uid">
-                <option value=""> choose </option>
-                <?php foreach($user as $u) {echo "<option value = $u->uid>$u->uname $u->usurname</option>";}?>
-                </select>
-            </label><br>
-            <label>ผลการตรวจ <input type="text" name="rs"/></label><br>
-            <label>โรงพยาบาล <select name="hid">
-                <option value=""> choose </option>
-                <?php foreach($h_List as $h) {echo "<option value = $h->Hid>$h->H_name</option>";}?>
-                </select>
-            </label><br>
-            
-            <div class="btn-group">
+        </table>
+           
+       <br>
+        <div class="btn-group center">
                 <input type="hidden" name="controller" value="results"/>
                 <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
                 <button class="button" type="submit" name="action" value="indexNurse">Back</button>
                 <button class="button" type="submit" name="action" value="addResultsNurse">Save</button>
             </div>
-        </form>
     </div>
+ </form>     
 </html>
