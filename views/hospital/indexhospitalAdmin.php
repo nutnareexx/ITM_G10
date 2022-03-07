b63aba256b3093392254a03f1615cd3cea2f10c2
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-08 02:22:50 +0700

    Update indexhospitalAdmin.php

--- views/hospital/indexhospitalAdmin.php
+++ views/hospital/indexhospitalAdmin.php
@@ -66,7 +66,7 @@
         #customers th {
             padding-top: 12px;
             padding-bottom: 12px;
-            text-align: left;
+            text-align: center;
             background-color: #0974ba;
             color: white;
         }
@@ -95,16 +95,41 @@
             color: black;
         }
 
+        .btn-group2 .button {
+            /*border-radius: 5%;*/
+            position: sticky ;
+            background-color: #0974ba ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 14px 16px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            float: right;
+        }
+
+        .btn-group2 .button:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group2 .button:hover {
+            /*background-color: #ddd;*/
+            color: black;
+        }
+
         input[type=text], select {
             font-family: 'Prompt', sans-serif;
-            width: 25%;
-            padding: 12px 10px;
+            width: 30%;
+            padding: 5px 10px;
             margin: 8px 0;
             font-size: 16px;
-            display: inline-block;
+            /*display: inline-block;*/
             border: 1px solid #ccc;
-            border-radius: 4px;
+            border-radius: 50px;
             box-sizing: border-box;
+            float: right;
         }
         .card {
                     font-family: 'Prompt', sans-serif;
@@ -122,52 +147,50 @@
             
     </style>
 
-<div class="header">
-        <h1> โรงพยาบาล</h1>
-</div>
-
 <div class="topnav">
                 <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
                 <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
                 <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
-                <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
+                <a href="?controller=user&action=indexAdmin&userid=<?php echo $userid?>" style="float:right">
                 <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+                <form method="get" action="">
+                <div class="btn-group2">
+                    <input type="text" name="key">
+                    <input type="hidden" name="controller" value="hospital"/>
+                    <input type="hidden" name="userid" value="<?php echo $userid ;?>"/>
+                    <button class="button" type="submit" name="action" value="search">
+                    <i class='fas fa-search' style='font-size: 24px'></i> </button>
+                </div>
+                </form>
 
                 <?php echo $userid?>
-            </div><br>
+            </div>
 
-
+            <br>
 <body>
     <center>
         <div class="try1">
             
-            <br>new Hospital <a href="?controller=hospital&action=newhospital&userid=<?php echo $userid;?>">Click</a><br>
-            <form method="get" action="">
-                <div class="btn-group">
-                    <input type="text" name="key">
-                    <input type="hidden" name="controller" value="hospital"/>
-                    <input type="hidden" name="userid" value="<?php echo $userid ;?>"/>
-                    <button class="button" type="submit" name="action" value="search"> Search </button>
-                </div>
-                </form>
+            <h3>new Hospital <a href="?controller=hospital&action=newhospital&userid=<?php echo $userid;?>">click!!!</a></h3>
+            
              </div>    
              
             <table border = 1 id="customers">
             <tr>
             <th><b>ชื่อโรงพยาบาล</th>
-            <th><b>เวลาเปิดทำการ</th>
-            <th><b>เวลาปิดทำการ</th>
+            <th><b>เวลาเปิด-ปิดทำการ</th>
             <th><b>ราคาตรวจ RT-PCR</th>
             <th><b>Update</th>
             <th><b>Delete</th> </tr>
             <?php foreach( $hospitalmodelList as $hospitallist)
             {
                 echo "<tr> <td>$hospitallist->H_name</td>
-                <td>$hospitallist->H_time_open</td>
-                <td>$hospitallist->H_time_close</td> 
+                <td>$hospitallist->H_time_open-$hospitallist->H_time_close</td>
                 <td>$hospitallist->H_price</td> 
-                <td>  <a href=?controller=hospital&action=updateforms&Hid=$hospitallist->Hid&userid=$userid> update </a> </td>
-                <td>  <a href=?controller=hospital&action=deleteconfirm&Hid=$hospitallist->Hid&userid=$userid> delete</a> </td></tr>";
+                <td align=center>  <a href=?controller=hospital&action=updateforms&Hid=$hospitallist->Hid&userid=$userid>
+                <i class='fas fa-edit' style='font-size: 24px;'></i> </a> </td>
+                <td align=center>  <a href=?controller=hospital&action=deleteconfirm&Hid=$hospitallist->Hid&userid=$userid>
+                <i class='fas fa-trash-alt' style='font-size: 24px;'></i></a> </td></tr>";
             }
             echo "</table>";
             ?>
