720d9ed7d8447260cb36bb4484cc1da4e219f8b4
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-08 02:23:29 +0700

    Update uploadResults.php

--- views/Results/uploadResults.php
+++ views/Results/uploadResults.php
@@ -1,79 +1,114 @@
 <html>
   <head> 
         <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
   </head>
  
   <style>
     .header {
-                font-family: 'Prompt', sans-serif;
-                background-color: #f1f1f1;
-                padding: 20px;
-                text-align: center;
-            }
+      font-family: 'Prompt', sans-serif;
+      background-color: #f1f1f1;
+      padding: 20px;
+      text-align: center;
+    }
 
-            .btn-group .button {
-                font-family: 'Prompt', sans-serif;
-                position: relative;
-                background-color: #0974ba ; /* Green */
-                border: 1px black;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                cursor: pointer;
-                /*float: center;*/
-            }
+    .topnav {
+            overflow: hidden;
+            background-color:#0974ba;
+        }
 
-            .btn-group .button:not(:last-child) {
-                border-right: none; /* Prevent double borders */
-            }
+    /* Style the topnav links */
+    .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
 
-            .btn-group .button:hover {
-                background-color: #ddd;
-                color: black;
-            }
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
 
-            .card2 {
-                font-family: 'Prompt', sans-serif;
-                background-color: #f1f1f1;
-                padding: 20px;
-                margin-top: 20px;
+    .btn-group .button {
+      font-family: 'Prompt', sans-serif;
+      position: relative;
+      background-color: #0974ba ; /* Green */
+      border: 1px black;
+      color: white;
+      padding: 10px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      cursor: pointer;
+      /*float: center;*/
+      border-radius: 50px;
+    }
+
+    .btn-group .button:not(:last-child) {
+      border-right: none; /* Prevent double borders */
+    }
+
+    .btn-group .button:hover {
+      background-color: #ddd;
+      color: black;
+     }
+
+    .card2 {
+      font-family: 'Prompt', sans-serif;
+      background-color: #f1f1f1;
+      padding: 20px;
+      
                
-            }
-            input[type=file], select {
-                font-family: 'Prompt', sans-serif;
-                width: 100%;
-                padding: 12px 20px;
-                margin: 8px 0;
-                display: inline-block;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                box-sizing: border-box;
-            }
+    }
+    input[type=file], select {
+      font-family: 'Prompt', sans-serif;
+      width: 80%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 50px;
+      box-sizing: border-box;
+    }
+
+    
   </style>
 
-<div class="header">
-  <h1> จองตรวจ RT-PCR 
-  </h1>
+<div class="topnav">
+  <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
+  <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+  <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+  <a href="?controller=user&action=indexAdmin&userid=<?php echo $userid?>" style="float:right">
+  <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+
+                <?php echo $userid?>
 </div>
 
 
 <div class="card2">
   <form action="add_file_db.php" method="post" enctype="multipart/form-data" name="upfile" id="upfile">
-
+    <center>
+      <div class="btn-group">
       <label>
-        <input type="file" name="fileupload" id="fileupload"  required="required"/><br>
+        <input class="button" type="file" name="fileupload" id="fileupload"  required="required"/><br>
       </label>
       <br>
-      <!--<input type="hidden" name="controller" value="results"/>-->
-      <div class="btn-group">
+  </div>
+  <div class="btn-group" >
       <input type="hidden" name="id" value="<?php echo $re->id;?>"/>
       <input type="hidden" name="uid" value="<?php echo $re->uid;?>"/>
       <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
       <button class="button" type="submit" name="button" id="button" value="upResult"> SUBMIT </button>
       </div>
+      <br>
   </form>
   <form method="get" action="">
     <div class="btn-group">
@@ -82,5 +117,7 @@
     <button class="button" type="submit" name="action" value="indexNurse">Back</button>
     </div>
   </form>
+    </center>
+  
 </div>
 </html>
