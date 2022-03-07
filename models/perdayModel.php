30d38589481fa4a03a0487f00262f1b3124cea11
nutnareexx <nutnaree.co@ku.th>
2022-03-08 03:24:13 +0700

    Create perdayModel.php

---
+++ models/perdayModel.php
@@ -0,0 +1,54 @@
+<?php
+class perdayModel {
+    public $mid;
+    public $d;
+    public $month;
+    public $year;
+    public $start;
+    public $end;
+    public $num;
+    public $hid;
+
+    public function __construct($id,$day,$m,$y,$s,$e,$n,$hid)
+    {
+        $this->mid = $id;
+        $this->d = $day;
+        $this->month = $m;
+        $this->year = $y;
+        $this->start = $s;
+        $this->end = $e;
+        $this->num = $n;
+        $this->hid = $hid;
+
+    }
+    public static function getdate($id)
+    {
+        $perList = [];
+        require("connection_connect.php");
+        $sql = "SELECT m.max_id,DAY(m.date) AS dday ,MONTH(m.date) AS mm,YEAR(m.date) AS yy,m.time_open,m.time_close,m.max,m.Hid 
+        FROM `max_per_day`AS m 
+        WHERE m.Hid='$id'
+        GROUP BY DAY(m.date),MONTH(m.date),YEAR(m.date);";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc()){
+            $mid = $my_row['max_id'];
+            $d = $my_row['dday'];
+            $month = $my_row['mm'];
+            $year = $my_row['yy'];
+            $start = $my_row['time_open'];
+            $end = $my_row['time_close'];
+            $num = $my_row['max'];
+            $hid = $my_row['Hid'];
+            $perList[] = new perdayModel($mid,$d,$month,$year,$start,$end,$num,$hid);
+        //echo $mid,$day,$month,$year,$start,$end,$num,$hid;
+        }
+    
+        
+        require("connection_close.php");
+        return $perList;
+        //echo $mid,$day,$month,$year,$start,$end,$num,$hid;
+
+        //return new perdayModel($mid,$day,$month,$year,$start,$end,$num,$hid);
+    }
+}
+?>
