8ebe72056744481dc0da8c3c8d6fdc295457a127
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-08 02:23:04 +0700

    Update index_maxperday.php

--- views/max_per_day/index_maxperday.php
+++ views/max_per_day/index_maxperday.php
@@ -6,6 +6,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
         <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
     </head>
 
     <style>
@@ -85,6 +86,7 @@
             font-size: 16px;
             cursor: pointer;
             /*float: center;*/
+            border-radius: 50px;
         }
 
         .btn-group .button:not(:last-child) {
@@ -96,57 +98,82 @@
             color: black;
         }
 
+        .btn-group2 .button {
+            /*border-radius: 5%;*/
+            position: sticky ;
+            background-color: #0974ba ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 14px 16px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            float: right;
+        }
+
+        .btn-group2 .button:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group2 .button:hover {
+            /*background-color: #ddd;*/
+            color: black;
+        }
+
         input[type=text], select {
             font-family: 'Prompt', sans-serif;
-            width: 25%;
-            padding: 12px 10px;
-            margin: 8px 0;
+            width: 30%;
+            padding: 5px 10px;
+            margin: 8px 0px;
             font-size: 16px;
-            display: inline-block;
+            /*display: inline-block;*/
             border: 1px solid #ccc;
-            border-radius: 4px;
+            border-radius: 50px;
             box-sizing: border-box;
+            float: right;
         }
 
     </style>
-
-    <div class="header">
-        <h1> จำนวนที่รับได้ต่อวัน </h1>
-    </div>
 
     <div class="topnav">
                 <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
                 <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
                 <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
-                <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
+                <a href="?controller=user&action=indexAdmin&userid=<?php echo $userid?>" style="float:right">
                 <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+                <form method="get" action="" >
+                    <div class="btn-group2">
+                        <input type="text" name="key" >
+                        
+                        <input type="hidden" name="controller" value="maxperday"/>
+                        <input type="hidden" name="userid" value="<?php echo $userid?>"/>
+                        
+                        <button class="button" type="submit" name="action" value="search"> 
+                        <i class='fas fa-search' style='font-size: 24px'></i> </button>
+                    </div>
+                        
+                    
+                </form>
 
                 <?php echo $userid?>
-            </div>
+    </div>
             <br>
 
     <body>
             
         <div class="try1">
             <h3>new max per day <a href="?controller=maxperday&action=newMaxperday&userid=<?php echo $userid?>"> click!!! </a> </h3>
+        </div>
 
-
-            <form method="get" action="">
-                <div class="btn-group">
-                <input type="text" name="key">
-                <input type="hidden" name="controller" value="maxperday"/>
-                <input type="hidden" name="userid" value="<?php echo $userid?>"/>
-                <button class="button" type="submit" name="action" value="search"> 
-            Search </button>
-                </div>
-            </form>
+            
             
         <center>
             <table border = 1 id="customers">
                 <tr> <th>โรงพยาบาล</th> 
                 <th>วันที่</th>
-                <th>เวลาเปิดทำการ</th>
-                <th>เวลาปิดทำการ</th>
+                <th>เวลาเปิด-ปิดทำการ</th>
                 <th>จำนวนที่รับได้ต่อวัน</th>
                 <th>update</th>
                 <th>delete</th></tr>
@@ -154,11 +181,12 @@
             {
                 echo "<tr> <td>$m->H_name</td>
                 <td>$m->max_date</td>
-                <td>$m->max_topen</td>
-                <td>$m->max_tclose</td>
+                <td>$m->max_topen-$m->max_tclose</td>
                 <td>$m->max_num</td>
-                <td> <a href=?controller=maxperday&action=updateForm&max_id=$m->max_id&userid=$userid>update</a></td>
-                <td> <a href=?controller=maxperday&action=deleteConfirm&max_id=$m->max_id&userid=$userid>delete</td>
+                <td align=center> <a href=?controller=maxperday&action=updateForm&max_id=$m->max_id&userid=$userid>
+                <i class='fas fa-edit' style='font-size: 24px;'></i></a></td>
+                <td align=center> <a href=?controller=maxperday&action=deleteConfirm&max_id=$m->max_id&userid=$userid>
+                <i class='fas fa-trash-alt' style='font-size: 24px;'></i></td>
                 </tr>";
             }
 
