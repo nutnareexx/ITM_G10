c891dd73c93257be408a33680e8054c4fad1a38b
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-10 01:33:15 +0700

    Update updateForm.php

--- views/max_per_day/updateForm.php
+++ views/max_per_day/updateForm.php
@@ -107,6 +107,8 @@
                 <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
                 <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
                 <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+                <a href="?controller=user&action=logout&userid=<?php echo $userid?>" style="float:right">
+                    <i class='fas fa-door-open' style='font-size:24px'></i></a>
                 <a href="?controller=user&action=indexAdmin&userid=<?php echo $userid?>" style="float:right">
                 <i class='fas fa-user-alt' style='font-size:24px'></i></a>
 
@@ -121,7 +123,7 @@
         <table align="center">
             <tr>
                 <td align="right">วันที่ : </td>
-                <td><label>  <input type="date" name="max_date"
+                <td><label>  <input type="date" name="max_date"  
                 value="<?php echo $m->max_date;?>"/></label></td>
             </tr>
             <tr>
@@ -145,6 +147,7 @@
                 <input type="hidden" name="controller" value="maxperday"/>
                 <input type="hidden" name="max_id" value="<?php echo $m->max_id;?>"/>
                 <input type="hidden" name="Hid" value="<?php echo $m->Hid;?>"/>
+                <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
                 <input type="hidden" name="userid" value="<?php echo $userid;?>"/>
                 <button class="button" type="submit" name="action" value="indexNurse">Back</button>
                 <button class="button" type="submit" name="action" value="update"> update </button>
