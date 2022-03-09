1005440ebf456925756379334446eee9c562f065
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-10 01:32:44 +0700

    Update index_datebook.php

--- views/booking/index_datebook.php
+++ views/booking/index_datebook.php
@@ -146,6 +146,8 @@
             <a href="?controller=booking&action=index&userid=<?php echo $userid?>"> จองตรวจ RT-PCR </a>
             <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
             <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+            <a href="?controller=user&action=logout&userid=<?php echo $userid?>" style="float:right">
+                    <i class='fas fa-door-open' style='font-size:24px'></i></a>
             <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
             <i class='fas fa-user-alt' style='font-size:24px'></i></a>
 
