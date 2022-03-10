03212d0746ca7a909f6716af1da399f119afb7bb
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-11 01:32:00 +0700

    Update newResults.php

--- views/Results/newResults.php
+++ views/Results/newResults.php
@@ -37,6 +37,7 @@
             
         }
         .btn-group .button {
+            font-family: 'Prompt', sans-serif;
             position: relative;
             background-color: #0974ba ; /* Green */
             border: 1px black;
