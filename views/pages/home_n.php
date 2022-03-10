ad471bc649d923c28370b2cd1f1199df4af60d70
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-11 01:04:40 +0700

    Update home_n.php

--- views/pages/home_n.php
+++ views/pages/home_n.php
@@ -54,7 +54,7 @@
     <a href="?controller=maxperday&action=index&userid=<?php echo $userid?>"> วันเวลาที่รับตรวจ </a>
     <a href="?controller=results&action=indexNurse&userid=<?php echo $userid?>"> ผลการตรวจ </a>
     <a href="?controller=hospital&action=indexAdmin&userid=<?php echo $userid?>"> โรงพยาบาล </a>
-    <a href="?controller=user&action=logout&userid=<?php echo $userid?>" style="float:right">
+    <a href="?controller=user&action=logoutUser&userid=<?php echo $userid?>" style="float:right">
                     <i class='fas fa-door-open' style='font-size:24px'></i></a>
     <a href="?controller=user&action=indexAdmin&userid=<?php echo $userid?>" style="float:right">
         <i class='fas fa-user-alt' style='font-size:24px'></i></a>
