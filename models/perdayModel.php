98189c1bbe26f7759f76039b0d178958ac9e8491
nutnareexx <nutnaree.co@ku.th>
2022-03-10 20:21:37 +0700

    Update perdayModel.php

--- models/perdayModel.php
+++ models/perdayModel.php
@@ -1,7 +1,7 @@
 <?php
 class perdayModel {
     public $mid;
-    public $d;
+    public $x;
     public $month;
     public $year;
     public $start;
@@ -9,27 +9,22 @@ class perdayModel {
     public $num;
     public $hid;
 
-    public function __construct($id,$day,$m,$y,$s,$e,$n,$hid)
+    public function __construct($id,$x,$n,$hid)
     {
         $this->mid = $id;
-        $this->d = $day;
-        $this->month = $m;
-        $this->year = $y;
-        $this->start = $s;
-        $this->end = $e;
+        $this->x = $x;
         $this->num = $n;
         $this->hid = $hid;
 
     }
-    public static function getdate($id)
+    public static function get($id)
     {
         $perList = [];
         require("connection_connect.php");
         $sql = "SELECT m.max_id,DAY(m.date) AS dday ,MONTH(m.date) AS mm,YEAR(m.date) AS yy,
-        TIME_FORMAT(m.time_open,'%H : %i') AS op,TIME_FORMAT(m.time_close,'%H:%i') AS cl,m.max,m.Hid 
+        TIME_FORMAT(m.time_open,'%H:%i') AS op,TIME_FORMAT(m.time_close,'%H:%i') AS cl,m.max,m.Hid 
         FROM `max_per_day`AS m 
-        WHERE m.Hid='$id'
-        GROUP BY DAY(m.date),MONTH(m.date),YEAR(m.date);";
+        WHERE m.Hid='$id'";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc()){
             $mid = $my_row['max_id'];
@@ -41,15 +36,73 @@ class perdayModel {
             $num = $my_row['max'];
             $hid = $my_row['Hid'];
             $perList[] = new perdayModel($mid,$d,$month,$year,$start,$end,$num,$hid);
-        //echo $mid,$day,$month,$year,$start,$end,$num,$hid;
         }
-    
+        require("connection_close.php");
+        return $perList;
+    }
+
+    public static function getd($id)
+    {
+        $perList = [];
+        require("connection_connect.php");
+        $sql = "SELECT m.max_id,DAY(m.date) AS dday ,m.max,m.Hid 
+        FROM `max_per_day`AS m 
+        WHERE m.Hid='$id'
+        GROUP BY DAY(m.date);";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc()){
+            $mid = $my_row['max_id'];
+            $x = $my_row['dday'];
+            $num = $my_row['max'];
+            $hid = $my_row['Hid'];
+            $perList[] = new perdayModel($mid,$x,$num,$hid);
+        }
+        require("connection_close.php");
+        return $perList;
+    }
+
+    public static function getm($id)
+    {
+        $perList = [];
+        require("connection_connect.php");
+        $sql = "SELECT m.max_id,MONTH(m.date) AS mm,m.max,m.Hid 
+        FROM `max_per_day`AS m 
+        WHERE m.Hid='$id'
+        GROUP BY MONTH(m.date);";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc()){
+            $mid = $my_row['max_id'];
+            $x = $my_row['mm'];
+            $num = $my_row['max'];
+            $hid = $my_row['Hid'];
+            $perList[] = new perdayModel($mid,$x,$num,$hid);
+        
+        }
         
         require("connection_close.php");
         return $perList;
-        //echo $mid,$day,$month,$year,$start,$end,$num,$hid;
+    }
 
-        //return new perdayModel($mid,$day,$month,$year,$start,$end,$num,$hid);
+    public static function gety($id)
+    {
+        $perList = [];
+        require("connection_connect.php");
+        $sql = "SELECT m.max_id,YEAR(m.date) AS mm,m.max,m.Hid 
+        FROM `max_per_day`AS m 
+        WHERE m.Hid='$id'
+        GROUP BY YEAR(m.date);";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc()){
+            $mid = $my_row['max_id'];
+            $x = $my_row['mm'];
+            $num = $my_row['max'];
+            $hid = $my_row['Hid'];
+            $perList[] = new perdayModel($mid,$x,$num,$hid);
+        
+        }
+        
+        require("connection_close.php");
+        return $perList;
     }
 }
 ?>
