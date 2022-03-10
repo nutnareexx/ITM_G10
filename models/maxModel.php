da15bd625f57b28757ce809af8c2a5e80205aeb0
nutnareexx <nutnaree.co@ku.th>
2022-03-11 01:54:27 +0700

    Update maxModel.php

--- models/maxModel.php
+++ models/maxModel.php
@@ -17,13 +17,13 @@ class maxModel {
         $this->hid = $hid;
 
     }
-    public static function get($id)
+    public static function getd($id)
     {
         $dmyList = [];
         require("connection_connect.php");
         $sql = "SELECT DATE_FORMAT(m.date,'%d') AS dday ,DATE_FORMAT(m.date,'%m') AS mm,DATE_FORMAT(m.date,'%Y')AS yy,m.Hid 
         FROM `max_per_day`AS m 
-        WHERE m.Hid='H001'
+        WHERE m.Hid='$id'
         GROUP BY DAY(m.date) ,MONTH(m.date),YEAR(m.date);";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc()){
@@ -37,5 +37,26 @@ class maxModel {
         require("connection_close.php");
         return $dmyList;
     }
+    public function gett($hid,$did){
+        $timeList = [];
+        require("connection_connect.php");
+        $sql = "SELECT DATE_FORMAT(m.date,'%d') AS dday ,DATE_FORMAT(m.date,'%m') AS mm,DATE_FORMAT(m.date,'%Y')AS yy,m.Hid 
+        FROM `max_per_day`AS m 
+        WHERE m.Hid='$hid' AND m.date='$did'
+        GROUP BY DAY(m.date) ,MONTH(m.date),YEAR(m.date);";
+        $result = $conn->query($sql);
+        while($my_row = $result->fetch_assoc()){
+            
+            $d = $my_row['dday'];
+            $month = $my_row['mm'];
+            $year = $my_row['yy'];
+            $hid = $my_row['Hid'];
+            $dmyList[] = new maxModel($d,$month,$year,$hid);
+        }
+        require("connection_close.php");
+        return $timeList;
+    }
+
+
 }
 ?>
