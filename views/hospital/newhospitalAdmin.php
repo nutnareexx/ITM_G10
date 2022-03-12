cc739025bc24006b0f54878a56026763862b08b1
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-13 02:24:09 +0700

    Update newhospitalAdmin.php

--- views/hospital/newhospitalAdmin.php
+++ views/hospital/newhospitalAdmin.php
@@ -35,7 +35,8 @@
             font-family: 'Prompt', sans-serif;
             background-color: #f1f1f1;
             padding: 20px;
-            
+            width: 50%;
+            border-radius: 10px;
         }
         .btn-group .button {
             font-family: 'Prompt', sans-serif;
@@ -89,6 +90,9 @@
         .center {
             text-align: center;
             border: none;
+            margin: auto;
+            width: 60%;
+            padding: 10px;
             }
 
             table, td {
@@ -108,7 +112,7 @@
                 <?php echo $userid?>
 </div>
 
-<div class="card">
+<div class="card center">
     <form method="get" action="">
         <table align="center">
             <tr>
