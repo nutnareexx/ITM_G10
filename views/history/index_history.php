e2547f98fafbeccfe80efcbff7acd7ff25928094
nutnareexx <nutnaree.co@ku.th>
2022-03-12 22:19:44 +0700

    Update index_history.php

--- views/history/index_history.php
+++ views/history/index_history.php
@@ -6,11 +6,11 @@
 
     <style>
         .header {
-            font-family: 'Prompt', sans-serif;
-            background-color: #f1f1f1;
-            padding: 40px;
-            text-align: center;
-        }
+                font-family: 'Prompt', sans-serif;
+                background-color: #f1f1f1;
+                padding: 40px;
+                text-align: center;
+            }
 
         /* Style the top navigation bar */
         .topnav {
@@ -34,23 +34,25 @@
             background-color: #ddd;
             color: black;
         }
-
+        
         #customers {
             font-family: 'Prompt', sans-serif;
             border-collapse: collapse;
             width: 80%;
-            
             }
 
         #customers td, #customers th {
             border: 1px solid #ddd;
             padding: 8px;
-            
             }
 
-        #customers tr:nth-child(even){background-color: #f2f2f2;}
+        #customers tr:nth-child(even){
+            background-color: #f2f2f2;
+        }
 
-        #customers tr:hover {background-color: #ddd;}
+        #customers tr:hover {
+            background-color: #80b0cf;
+        }
 
         #customers th {
             padding-top: 12px;
@@ -59,6 +61,19 @@
             background-color: #0974ba;
             color: white;
             }
+
+
+        .try1{
+            font-family: 'Prompt', sans-serif;
+            text-align: center;
+            
+        }
+
+        .try2{
+            font-family: 'Prompt', sans-serif;
+            padding: 20px;
+            text-align: center;
+        }
 
         .btn-group .button {
             /*border-radius: 5%;*/
@@ -84,153 +99,64 @@
             color: black;
         }
 
-        .card2 {
-                font-family: 'Prompt', sans-serif;
-                background-color: #f1f1f1;
-                padding: 20px;
-                margin-top: 50px;
-                margin-left: auto;
-                margin-right: auto;
-            }
-        
-        .card3 {
-                font-family: 'Prompt', sans-serif;
-                background-color: #f1f1f1;
-                font-size: 15px;
-                padding: 10px 10px 10px 10px;
-                margin: 10px 10px 10px 10px;
-            }
-
-        .circle{
-                text-align: center;
-                background-color: #f1f1f1;
-                padding: 10px 10px 10px 10px;
-                margin: 10px 10px 10px 10px;
-             
-            }
-        
-        
-        
-        .leftcolumn {   
-                float: left;
-                width: 65%;
-                
-            }
-
-            /* Right column */
-        .rightcolumn {
-                float: right;
-                width: 29%;
-                background-color: #f1f1f1;
-                /*padding-left: 20px;*/
-                /*margin-top: 20px;*/
-            }
-
-            /* Clear floats after the columns */
-        .row:after {
-                content: "";
-                display: table;
-                clear: both;
-            }
-
-        .try2{
-            text-align:center;
-            font-family: 'Prompt', sans-serif;
-        }
-
-        table {
-            font-family: 'Prompt', sans-serif;
-            background-color: #f1f1f1;
-            border-radius: 10px;
-            padding: 20px 20px 20px 20px;
-            margin-top: 20px;
-            margin-bottom: 20px;
-            border-collapse: collapse;
-            width: 80%;
-        }
-
-        th, td {
-            text-align: center;
-            padding: 8px;
-        }
-
-        th{
-            padding-top: 12px;
-            padding-bottom: 12px;
-            text-align: center;
-            background-color: #0974ba;
-            color: white;
-        }
-
-        #cus table {
-            font-family: 'Prompt', sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
-        
-      
-        
-        #cus td:hover {
-            background-color: #ddd;
-        }
-
-        .button {
-            position: relative;
-            background-color: #0974ba ; 
+        .btn-group2 .button {
+            /*border-radius: 5%;*/
+            position: sticky ;
+            background-color: #0974ba ; /* Green */
             border: 1px black;
             color: white;
-            width: 100%;
-            height: 100%;
-            /* padding: 10px; */
+            padding: 14px 16px;
             text-align: center;
             text-decoration: none;
             display: inline-block;
             font-size: 16px;
             cursor: pointer;
-            /*float: center;*/
-            border-radius: 50px;
+            float: right;
         }
 
-        .button:not(:last-child) {
+        .btn-group2 .button:not(:last-child) {
             border-right: none; /* Prevent double borders */
         }
 
-        .button:hover {
-            background-color: #ddd;
+        .btn-group2 .button:hover {
+            /*background-color: #ddd;*/
             color: black;
         }
 
-
-        
-        
-
         input[type=text], select {
-                font-family: 'Prompt', sans-serif;
-                width: 95%;
-                padding: 20px 20px 20px 20px;
-                margin-top: 20px ;
-                display: inline-block;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                box-sizing: border-box;
-                font-size: 16;
-            }
+            font-family: 'Prompt', sans-serif;
+            width: 30%;
+            padding: 5px 10px;
+            margin: 8px 0;
+            font-size: 16px;
+           /* display: inline-block;*/
+            border: 1px solid #ccc;
+            border-radius: 50px;
+            box-sizing: border-box;
+            float: right;
+        }
         
             
     </style>
 
-            <div class="topnav">
-    
-            <a href="?controller=booking&action=index&userid=<?php echo $userid;?>"> จองตรวจ RT-PCR </a>
-            <a href="?controller=results&action=indexUser&userid=<?php echo $userid;?>"> ผลการตรวจ </a>
-            <a href="?controller=history&action=index&userid=<?php echo $userid;?>"> ประวัติการจอง </a>
-            <a href="?controller=hospital&action=indexUser&userid=<?php echo $userid;?>"> โรงพยาบาล </a>
-            <a href="?controller=user&action=logoutUser&userid=<?php echo $userid?>" style="float:right">
+<div class="topnav">
+                <a href="?controller=booking&action=index&userid=<?php echo $userid?>"> จองตรวจ RT-PCR </a>
+                <a href="?controller=results&action=indexUser&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+                <a href="?controller=history&action=index&userid=<?php echo $userid;?>"> ประวัติการจอง </a>
+                <a href="?controller=hospital&action=indexUser&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+                <a href="?controller=user&action=logoutUser&userid=<?php echo $userid?>" style="float:right">
                     <i class='fas fa-door-open' style='font-size:24px'></i></a>
-            <a href="?controller=user&action=indexUser&userid=<?php echo $userid;?>" style="float:right">
-            <i class='fas fa-user-alt' style='font-size:24px'></i></a>
-
-        
-
-        </div>
+                <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
+                <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+                <form method="get" action="">
+                <div class="btn-group2">
+                <input type="text" name="key">
+                    <input type="hidden" name="controller" value="hospital"/>
+                    <input type="hidden" name="userid" value="<?php echo $userid?>"/>
+                    <button class="button" type="submit" name="action" value="searchuser"> 
+                    <i class='fas fa-search' style='font-size: 24px'></i> </button>
+                </div>    
+                </form>
+                
+            </div>
 </html>
