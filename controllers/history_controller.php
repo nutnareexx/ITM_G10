a7d264f14760ecd6963a0e341c701bba16a012c9
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-14 01:22:04 +0700

    Update history_controller.php

--- controllers/history_controller.php
+++ controllers/history_controller.php
@@ -16,5 +16,29 @@ class historyController{
         require_once('views/history/index_history.php');
         // historyController::index();
     }
+
+    public function indexNurse() {
+        $userid = $_GET['userid'];
+        $showallList = dateModel::getdate();
+        require_once('views/history/historyForNurse.php');
+    }
+
+    public function historyN() {
+        $userid = $_GET['userid'];
+        $date = $_GET['date'];
+        $dateList = showallModell::getAll($date);
+        require_once('views/history/historydetail.php');
+    }
+
+    public function searchN()
+    {
+        $key = $_GET['key'];
+        $date = $_GET['date'];
+        $userid = $_GET['userid'];
+        $dateList = showallModell::search($key,$date);
+        //require_once('views/hospital/indexhospital.php');
+        require_once('views/history/historydetail.php');
+        // historyController::index();
+    }
 }
 ?>
