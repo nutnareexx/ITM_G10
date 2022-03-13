4d78d0fcdb999329c4cc607619794e0e3ce06c6f
nutnareexx <nutnaree.co@ku.th>
2022-03-13 19:48:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ else{
 <html>
 <head></head>
 <body>
-    <?php echo "controller = ".$controller.", action = ".$action ;?>
-    <br>
+    <!-- <?php echo "controller = ".$controller.", action = ".$action ;?> -->
+    <!-- <br>
     [<a href="?controller=pages&action=home"> HOME </a>]
 
     [<a href="?controller=maxperday&action=index"> MAX_PER_DAY </a>]
     [<a href="?controller=results&action=index"> Results naaaaa </a>]
     [<a href="?controller=hospital&action=index"> HOSPITAL </a>]
     [<a href="?controller=user&action=indexlogin"> LOG IN </a>]
-    [<a href="?controller=booking&action=index"> การจอง </a>]
+    [<a href="?controller=booking&action=index"> การจอง </a>] -->
     <?php require_once("routes.php");?>
 </body>
 
