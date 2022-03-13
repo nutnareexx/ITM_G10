819d380dbfa9d1dd154d64860289bfaec8999e90
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-13 22:29:25 +0700

    Create waitForResult.php

---
+++ views/Results/waitForResult.php
@@ -0,0 +1,168 @@
+<html>
+    <head> 
+        <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    </head>
+
+    <style>
+        .header {
+            font-family: 'Prompt', sans-serif;
+            background-color: #f1f1f1;
+            padding: 40px;
+            text-align: center;
+        }
+
+        /* Style the top navigation bar */
+        .topnav {
+            overflow: hidden;
+            background-color: #0974ba;
+        }
+
+        /* Style the topnav links */
+        .topnav a {
+            font-family: 'Prompt', sans-serif;
+            float: left;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change color on hover */
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        #customers {
+            font-family: 'Prompt', sans-serif;
+            border-collapse: collapse;
+            width: 80%;
+            border-radius: 10px;
+            }
+
+        #customers td, #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+            }
+
+        #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+        #customers tr:hover {background-color: #ddd;}
+
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: center;
+            background-color: #0974ba;
+            color: white;
+            }
+
+        .btn-group .button {
+            /*border-radius: 5%;*/
+            position: relative;
+            background-color: #0974ba ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+       
+        }
+
+        .btn-group .button:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group .button:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .card2 {
+                font-family: 'Prompt', sans-serif;
+                background-color: #f1f1f1;
+                padding: 20px;
+                width: 50%;
+                border-radius: 10px;
+                
+            }
+        
+        .card3 {
+                font-family: 'Prompt', sans-serif;
+                background-color: #f1f1f1;
+                font-size: 15px;
+                padding: 10px 10px 10px 10px;
+                margin: 10px 10px 10px 10px;
+            }
+
+        .circle{
+                text-align: center;
+                background-color: #f1f1f1;
+                padding: 10px 10px 10px 10px;
+                margin: 10px 10px 10px 10px;
+             
+            }
+        
+        
+        
+        .leftcolumn {   
+                float: left;
+                width: 65%;
+                
+            }
+
+            /* Right column */
+        .rightcolumn {
+                float: right;
+                width: 29%;
+                background-color: #f1f1f1;
+                /*padding-left: 20px;*/
+                /*margin-top: 20px;*/
+            }
+
+            /* Clear floats after the columns */
+        .row:after {
+                content: "";
+                display: table;
+                clear: both;
+            }
+        
+        .center {
+            text-align: center;
+            border: none;
+            margin: auto;
+            width: 60%;
+            padding: 10px;
+
+            }
+            
+    </style>
+    
+    <div class="topnav">
+        <a href="?controller=booking&action=index&userid=<?php echo $userid?>"> จองตรวจ RT-PCR </a>
+        <a href="?controller=results&action=indexUser&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+        <a href="?controller=history&action=index&userid=<?php echo $userid;?>"> ประวัติการจอง </a>
+        <a href="?controller=hospital&action=indexUser&userid=<?php echo $userid?>"> โรงพยาบาล </a>
+        <a href="?controller=user&action=logoutUser&userid=<?php echo $userid?>" style="float:right">
+                    <i class='fas fa-door-open' style='font-size:24px'></i></a>
+        <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
+            <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+
+        
+    </div><br>
+            
+
+    <div class="card2 center">        
+            <h2>ยังไม่มีผลการตรวจ!!</h2>
+            <h3>กรุณารอผลการตรวจ</h3>
+            
+    </div>
+    
+    
+</html>
+    
