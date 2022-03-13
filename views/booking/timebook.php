175a9e0cfffb5dd1628521172aa014d066bb52be
wtmllll <91516571+wtmllll@users.noreply.github.com>
2022-03-13 23:37:44 +0700

    Update timebook.php

--- views/booking/timebook.php
+++ views/booking/timebook.php
@@ -84,6 +84,32 @@
             color: black;
         }
 
+        .btn-group2 .button2 {
+            font-family: 'Prompt', sans-serif;
+            /*border-radius: 5%;*/
+            position: relative;
+            background-color: #0974ba ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 10px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            /*float: center;*/
+            border-radius: 50px;
+        }
+
+        .btn-group2 .button2:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group2 .button2:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
         .card2 {
                 font-family: 'Prompt', sans-serif;
                 background-color: #f1f1f1;
@@ -208,23 +234,7 @@
             background-color: #ddd;
             color: black;
         }
-
-         /* a:link, a:visited {
-            background-color: #f44336;
-            color: white;
-            padding: 14px 25px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-        }
-
-         a:hover, a:active {
-            background-color: red;
-        } */
-
-        
-        
-
+ 
         input[type=text], select {
                 font-family: 'Prompt', sans-serif;
                 width: 95%;
@@ -248,7 +258,7 @@
             <a href="?controller=hospital&action=indexUser&userid=<?php echo $userid;?>"> โรงพยาบาล </a>
             <a href="?controller=user&action=logoutUser&userid=<?php echo $userid?>" style="float:right">
                     <i class='fas fa-door-open' style='font-size:24px'></i></a>
-            <a href="?controller=user&action=indexUser&userid=<?php echo $userid;?>" style="float:right">
+            <a href="?controller=user&action=index&userid=<?php echo $userid;?>" style="float:right">
             <i class='fas fa-user-alt' style='font-size:24px'></i></a>
 
         
