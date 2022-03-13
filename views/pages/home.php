94fded7570a068535d934bfdf39dde85cf0296db
nutnareexx <nutnaree.co@ku.th>
2022-03-13 19:48:37 +0700

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,10 +1,47 @@
 <html>
+
     <head> 
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" >
+        <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
         <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    </head>
+
     <style>
-        
-        /* Font */
+        html { 
+            /* background-color: lightblue; */
+            background-image: url("pic/n2.jpg");
+            /* background-size:  100% 100%; */
+
+        }
+        /* img {
+            text-align: center;
+            background-color: none;
+            padding: 10px 10px 10px 10px;
+            margin: 10px 10px 10px 10px;
+        } */
+
+        /* Style the top navigation bar */
+        .topnav {
+                overflow: hidden;
+                background-color: #0974ba;
+            }
+
+            /* Style the topnav links */
+        .topnav a {
+                font-family: 'Prompt', sans-serif;
+                float: left;
+                display: block;
+                color: #f2f2f2;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            /* Change color on hover */
+        .topnav a:hover {
+                background-color: #ddd;
+                color: black;
+            }
+
         .try2{
             text-align:center;
             font-family: 'Prompt', sans-serif;
@@ -12,57 +49,183 @@
 
         .header {
             font-family: 'Prompt', sans-serif;
-            background-color: #f1f1f1;
-            padding: 40px;
+            /* background-color: #f1f1f1; */
+            padding: 20px;
             text-align: center;
         }
 
-        /* Style the top navigation bar */
-        .topnav {
-            overflow: hidden;
-            background-color: #0974ba;
-        }
-
-        /* Style the topnav links */
-        .topnav a {
+        .btn-group .button {
             font-family: 'Prompt', sans-serif;
-            float: left;
-            display: block;
-            color: #f2f2f2;
+            /*border-radius: 5%;*/
+            position: relative;
+            background-color: #0974ba ; /* Green */
+            border: 1px black;
+            color: white;
+            padding: 15px 32px;
             text-align: center;
-            padding: 14px 16px;
             text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            /*float: center;*/
+            box-shadow: 3px 2px 8px gray;
+            border-radius: 50px;
         }
 
-        /* Change color on hover */
-        .topnav a:hover {
+        .btn-group .button:not(:last-child) {
+            border-right: none; /* Prevent double borders */
+        }
+
+        .btn-group .button:hover {
             background-color: #ddd;
             color: black;
         }
+
+        .card {
+            text-align: left;
+            font-family: 'Prompt', sans-serif;
+            /*font-size: 16px;*/
+        /* background-color: white;*/
+            padding: 70px;
+            margin-top: 10px;
+            margin-left: 100px;
+            
+        }
+
+        .card2 {
+            font-family: 'Prompt', sans-serif;
+            text-align: center;
+            /* background-color: #f1f1f1; */
+            padding: 20px;
+            margin-top: 50px;
+            /* margin-left: auto;
+            margin-right: auto; */
+        
+        }
+
+        .leftcolumn {
+            font-family: 'Prompt', sans-serif;
+                    float: left;
+                    width: 48%;
+                    margin-top: 100px;
+                    text-align: center;
+                    /*text-shadow: 3px 2px 6px black;*/
+                    
+                }
+
+        /* Right column */
+        .rightcolumn {
+                    float: right;
+                    margin-top: 50px;
+                    width: 48%;
+                    
+                    /*padding-left: 20px;
+                    margin-top: 20px;*/
+                }
+
+        /* Clear floats after the columns */
+        .row:after {
+                    content: "";
+                    display: table;
+                    clear: both;
+                }
+
+        input[type=text], select {
+            font-family: 'Prompt', sans-serif;
+            width: 100%;
+            padding: 12px 10px;
+            margin: 8px 0;
+            font-size: 16px;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 50px;
+            box-sizing: border-box;
+            box-shadow: 3px 2px 8px #837d7d;
+        }
+
+        input[type=password], select {
+            font-family: 'Prompt', sans-serif;
+            width: 100%;
+            padding: 12px 10px;
+            margin: 8px 0;
+            font-size: 16px;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 50px;
+            box-sizing: border-box;
+            box-shadow: 3px 2px 8px #837d7d;
+        }
+
         
 
     </style>
-</head>
 
+    <body>
+        
 
+        <!-- <div class="topnav">
+            <a href="?controller=booking&action=index&userid=<?php echo $userid?>"> จองตรวจ RT-PCR </a>
+            <a href="?controller=results&action=indexUser&userid=<?php echo $userid?>"> ผลการตรวจ </a>
+            <a href="?controller=history&action=index&userid=<?php echo $userid;?>"> ประวัติการจอง </a>
+            <a href="?controller=hospital&action=index"> โรงพยาบาล </a>
+            
 
+        
+        </div> -->
+        <div class="header">
+                <h1>HOME</h1>
+            </div>
 
+    <div class="row">
+        <div class="leftcolumn">
+                <div class="card">
+                    <form method="get" action="">
+                    <div class="btn-group" align="center"> 
+                        <table>
+                            <tr>
+                                <td align="right">เลขบัตรประชาชน : </td>
+                                <td> <label>  <input type="text"  name="username"/> </label></td>
+                            </tr>
+                            <tr>
+                                <td align="right">รหัสผ่าน : </td>
+                                <td><label>  <input type="password" name="password"/> </label></td>
+                            </tr>
+                        
+                        </table> <br>
+            
+                        <input type="hidden" name="controller" value="user"/>
+                        <button class="button" type="submit" name="action" value="login" id="myBtn"> เข้าสู่ระบบ </button><br><br>
 
-<body>
-    <br>
+                                สมัครสมาชิกใหม่ <?php echo "<a href=?controller=user&action=signin> SIGN UP
+                                <i class='fas fa-sign-in-alt' style='font-size:18px'></i> </a> ";?>
+                    
+                    </div>  
+                </form>       
+                </div>
+        </div>    
 
-    <div class="topnav">
-    <a href="?controller=booking&action=index&userid=<?php echo $userid?>"> จองตรวจ RT-PCR </a>
-    <a href="?controller=results&action=indexUser&userid=<?php echo $userid?>"> ผลการตรวจ </a>
-    <a href="?controller=history&action=index&userid=<?php echo $userid;?>"> ประวัติการจอง </a>
-    <a href="?controller=hospital&action=indexUser&userid=<?php echo $userid?>"> โรงพยาบาล </a>
-    <a href="?controller=user&action=logoutNurse&userid=<?php echo $userid?>" style="float:right">
-                    <i class='fas fa-door-open' style='font-size:24px'></i></a>
-    <a href="?controller=user&action=index&userid=<?php echo $userid?>" style="float:right">
-            <i class='fas fa-user-alt' style='font-size:24px'></i></a>
+        <div class="rightcolumn">
+            
+            <!-- <div class="img">
+                <img src="pic/amico.png" width="500" height="500">
+            </div> -->
+        </div>
 
-    </div>
+        <!-- <br><br> 
+                     <div class="card">
+                        username : 12345 password : 12345 General
+                        <br>
+                        username : 888 password : admin Admin
+                        
+                    </div> -->
+                       
+                    <!-- <br><br> -->
+        
 
-</body>
+            
+            
+        <!-- </form> -->
 
+    </div>         
+    </body>
 </html>
