cb6d38c1822fb3f125d233b8c3ac0c1e3beec7b5
nutnareexx <nutnaree.co@ku.th>
2022-03-14 03:52:22 +0700

    Update fileModel.php

--- models/fileModel.php
+++ models/fileModel.php
@@ -5,29 +5,40 @@ class fileModel{
     public $fname;
     public $fdate;
     public $frid;
+    public $re;
+    public $hname;
 
 
-public function __construct($id,$name,$date,$rid){
+public function __construct($id,$name,$date,$rid,$re,$n){
     $this->fid = $id;
     $this->fname =$name;
     $this->fdate = $date;
     $this->frid = $rid;
+    $this->re = $re;
+    $this->hname = $n;
 }
 
-public static function get($rid)
+public static function get($uid)
 {
+    $rList = [];
     require("connection_connect.php");
-    $sql = " SELECT * FROM `uploadfile` WHERE r_id ='$rid' ";
+    $sql = " SELECT u.fileID,u.fileupload,u.dateup,u.r_id,r.results,h.H_name FROM `uploadfile`AS u NATURAL JOIN results AS r NATURAL JOIN hostpital AS h
+    WHERE r.user_id = '$uid'; ";
     $result = $conn->query($sql);
-    $my_row = $result->fetch_assoc();
-    $fid = $my_row['fileID'];
-    $fname = $my_row['fileupload'];   
-    $fdate = $my_row['dateup'];
-    $frid = $my_row['r_id'];
+    while($my_row = $result->fetch_assoc()){
+        $fid = $my_row['fileID'];
+        $fname = $my_row['fileupload'];   
+        $fdate = $my_row['dateup'];
+        $frid = $my_row['r_id'];
+        $re = $my_row['results'];
+        $hname = $my_row['H_name'];
+        $rList[] = new fileModel($fid,$fname,$fdate,$frid,$re,$hname);
+    }
+    
     
     require("connection_close.php");
 
-    return new fileModel($fid,$fname,$fdate,$frid);
+    return $rList;
 }
 
 }
