25bb52671e1c0d49007cfbd801ddb95607721f5a
nutnareexx <nutnaree.co@ku.th>
2022-03-14 13:58:24 +0700

    Update fileModel.php

--- models/fileModel.php
+++ models/fileModel.php
@@ -22,13 +22,13 @@ public static function get($uid)
 {
     $rList = [];
     require("connection_connect.php");
-    $sql = " SELECT u.fileID,u.fileupload,u.dateup,u.r_id,r.results,h.H_name FROM `uploadfile`AS u NATURAL JOIN results AS r NATURAL JOIN hostpital AS h
+    $sql = " SELECT u.fileID,u.fileupload,DATE_FORMAT(u.dateup,'%d/%m/%Y') AS d,u.r_id,r.results,h.H_name FROM `uploadfile`AS u NATURAL JOIN results AS r NATURAL JOIN hostpital AS h
     WHERE r.user_id = '$uid'; ";
     $result = $conn->query($sql);
     while($my_row = $result->fetch_assoc()){
         $fid = $my_row['fileID'];
         $fname = $my_row['fileupload'];   
-        $fdate = $my_row['dateup'];
+        $fdate = $my_row['d'];
         $frid = $my_row['r_id'];
         $re = $my_row['results'];
         $hname = $my_row['H_name'];
